0eb950c74d0821aa6e0e472f703283cc2b907037
Matt Leotta <matt.leotta@kitware.com>
2013-09-26 11:48:34 -0400

    added initial test files

---
+++ tests/core/test_point_2d.cxx
@@ -0,0 +1,25 @@
+/*ckwg +5
+ * Copyright 2013 by Kitware, Inc. All Rights Reserved. Please refer to
+ * KITWARE_LICENSE.TXT for licensing information, or contact General Counsel,
+ * Kitware, Inc., 28 Corporate Drive, Clifton Park, NY 12065.
+ */
+
+#include <maptk/core/point_2d.h>
+
+
+// Put everything in an anonymous namespace so that different tests
+// won't conflict.
+namespace {
+
+using namespace maptk;
+
+
+} // end anonymous namespace
+
+int test_point_2d( int argc, char* argv[] )
+{
+  point_2d pt(10.0, 33.3);
+
+}
+
+
