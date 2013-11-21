df430d2c9f4884df124660ad5be59f49cd8b168e
Paul Tunison <paul.tunison@kitware.com>
2013-11-21 16:29:19 -0500

    Fixed typo and grammar error.

--- cmake/tools/maptk-configure-helper.cmake
+++ cmake/tools/maptk-configure-helper.cmake
@@ -13,8 +13,8 @@ endif()
 # There are TWO configures here on purpose. The second configure containing
 # the COPYONLY flag, only copies the file if the source and dest file are
 # different (equivalent to ``cmake -E copy_if_different``). This helps prevent
-# files from be touched durning a forced configuration when none of the
-# contained information changed (prevents rebuilding of dependee targets).
+# files from be touched during a forced configuration when none of the
+# contained information changed (prevents rebuilding of dependant targets).
 configure_file(
   "${__SOURCE_PATH__}"
   "${__TEMP_PATH__}"
