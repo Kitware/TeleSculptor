6b53aee16bb1f0c71218fcd0fdf50d4558e3a5ad
Paul Tunison <paul.tunison@kitware.com>
2013-11-21 10:09:23 -0500

    Removed unused code, added descriptive comment in config helper tool

--- cmake/tools/maptk-configure-helper.cmake
+++ cmake/tools/maptk-configure-helper.cmake
@@ -3,23 +3,6 @@
 # ``__SOURCE_PATH__`` and ``__TEMP_PATH__``
 #
 
-# Well this is redundant...
-#foreach(i RANGE 1 ${CMAKE_ARGC})
-#  #message(STATUS "arg: '${CMAKE_ARGV${i}}'")
-#  string(REGEX MATCH "-D([a-zA-Z0-9_]*)=(.*)" re_match "${CMAKE_ARGV${i}}")
-#  #message(STATUS "match 0: ${CMAKE_MATCH_0}")
-#  #message(STATUS "match 1: ${CMAKE_MATCH_1}")
-#  #message(STATUS "match 2: ${CMAKE_MATCH_2}")
-#  if(NOT (   ("${CMAKE_MATCH_1}" STREQUAL "")
-#          OR ("${CMAKE_MATCH_1}" STREQUAL "__OUTPUT_PATH__")
-#          OR ("${CMAKE_MATCH_1}" STREQUAL "__SOURCE_PATH__")
-#          OR ("${CMAKE_MATCH_1}" STREQUAL "__TEMP_PATH__")
-#          ))
-#    set(${CMAKE_MATCH_1} "${CMAKE_MATCH_2}")
-#    message(STATUS "${CMAKE_MATCH_1} := \"${CMAKE_MATCH_2}\"")
-#  endif()
-#endforeach()
-
 message(STATUS "Source Path       : '${__SOURCE_PATH__}'")
 message(STATUS "Intermediate Path : '${__TEMP_PATH__}'")
 message(STATUS "Output Path       : '${__OUTPUT_PATH__}'")
@@ -27,8 +10,11 @@ if(NOT EXISTS "${__SOURCE_PATH__}")
   message(FATAL_ERROR "Source file for configuration did not exist! -> ${__SOURCE_PATH__}")
 endif()
 
-# Performing a double configure. The second configure, the COPYONLY, only
-# replaces the destination file if the contents differ.
+# There are TWO configures here on purpose. The second configure containing
+# the COPYONLY flag, only copies the file if the source and dest file are
+# different (equivalent to ``cmake -E copy_if_different``). This helps prevent
+# files from be touched durning a forced configuration when none of the
+# contained information changed (prevents rebuilding of dependee targets).
 configure_file(
   "${__SOURCE_PATH__}"
   "${__TEMP_PATH__}"
