c3c57a6147b3e850d9e1a587c3924de891e8f739
Paul Tunison <paul.tunison@kitware.com>
2013-11-25 12:03:53 -0500

    Added warning suppression for unused variable
    When in release mode, which adds optimization to strip out assert calls,
    the variable assigned to the return code of the transformation call is
    then not used (only used in comparison within assert call, which got
    stripped). Added a trivial use of the variable after assert to suppress
    warning in release mode.

--- maptk/proj/geo_map.cxx
+++ maptk/proj/geo_map.cxx
@@ -76,6 +76,7 @@ proj_geo_map
   int ret = pj_transform(pj_utm, pj_latlon, 1, 1,
                          &lon, &lat, NULL);
   assert(ret == 0);
+  ret = 0;  // suppresses unused variable warning when build as release
 
   // lat/lon now in radians, so need to convert to degrees
   lat /= DEG_TO_RAD;
@@ -112,6 +113,7 @@ proj_geo_map
   int ret = pj_transform(pj_latlon, pj_utm, 1, 1,
                          &easting, &northing, NULL);
   assert(ret == 0);
+  ret = 0;  // suppresses unused variable warning when build as release
 
   // proj always returns with respect to the northern hemisphere.
   north_hemi = true;
