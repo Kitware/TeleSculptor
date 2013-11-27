b0e1f6a2641cc274032c4be13d3e895570350065
Paul Tunison <paul.tunison@kitware.com>
2013-11-27 12:15:00 -0500

    Added exception classes for upcoming config io utilities

---
+++ maptk/core/exceptions/config_io.cxx
@@ -0,0 +1,74 @@
+/*ckwg +5
+ * Copyright 2011-2013 by Kitware, Inc. All Rights Reserved. Please refer to
+ * KITWARE_LICENSE.TXT for licensing information, or contact General Counsel,
+ * Kitware, Inc., 28 Corporate Drive, Clifton Park, NY 12065.
+ */
+
+#include "config_io.h"
+
+#include <sstream>
+
+namespace maptk
+{
+
+config_io_exception
+::config_io_exception(char const* reason) MAPTK_NOTHROW
+  : maptk_core_base_exception()
+  , m_reason(reason)
+{
+}
+
+config_io_exception
+::~config_io_exception() MAPTK_NOTHROW
+{
+}
+
+file_not_found_exception
+::file_not_found_exception(path_t const& file_path, char const* reason) MAPTK_NOTHROW
+  : config_io_exception(reason)
+  , m_file_path(file_path)
+{
+  std::ostringstream sstr;
+  sstr << "Could not file file at location \'" << m_file_path.c_str() << "\': "
+          << m_reason;
+  m_what = sstr.str();
+}
+
+file_not_found_exception
+::~file_not_found_exception() MAPTK_NOTHROW
+{
+}
+
+file_not_read_exception
+::file_not_read_exception(path_t const& file_path, char const* reason) MAPTK_NOTHROW
+  : config_io_exception(reason)
+  , m_file_path(file_path)
+{
+  std::ostringstream sstr;
+  sstr << "Failed to read from file \'" << m_file_path.c_str() << "\': "
+       << m_reason;
+  m_what = sstr.str();
+}
+
+file_not_read_exception
+::~file_not_read_exception() MAPTK_NOTHROW
+{
+}
+
+file_write_exception
+::file_write_exception(path_t const& file_path, char const* reason) MAPTK_NOTHROW
+  : config_io_exception(reason)
+  , m_file_path(file_path)
+{
+  std::ostringstream sstr;
+  sstr << "Failed to write to file \'" << m_file_path.c_str() << "\': "
+       << m_reason;
+  m_what = sstr.str();
+}
+
+file_write_exception
+::~file_write_exception() MAPTK_NOTHROW
+{
+}
+
+}
---
+++ maptk/core/exceptions/config_io.h
@@ -0,0 +1,85 @@
+/*ckwg +5
+ * Copyright 2011-2013 by Kitware, Inc. All Rights Reserved. Please refer to
+ * KITWARE_LICENSE.TXT for licensing information, or contact General Counsel,
+ * Kitware, Inc., 28 Corporate Drive, Clifton Park, NY 12065.
+ */
+
+#ifndef MAPTK_CORE_EXCEPTIONS_CONFIG_IO_H
+#define MAPTK_CORE_EXCEPTIONS_CONFIG_IO_H
+
+#include "base.h"
+#include <maptk/core/types.h>
+
+namespace maptk
+{
+
+class MAPTK_CORE_EXPORT config_io_exception
+  : public maptk_core_base_exception
+{
+  public:
+    /**
+     * \brief Constructor
+     * \param reason Reason for the exception.
+     */
+    config_io_exception(char const* reason) MAPTK_NOTHROW;
+    /// Deconstructor
+    virtual ~config_io_exception() MAPTK_NOTHROW;
+
+    /// Reason for exception
+    std::string m_reason;
+};
+
+class MAPTK_CORE_EXPORT file_not_found_exception
+  : public config_io_exception
+{
+  public:
+    /**
+     * \brief Constructor
+     * \param file_path The file path that was looked for.
+     * \param reason    The reason the file wasn't found.
+     */
+    file_not_found_exception(path_t const& file_path, char const* reason) MAPTK_NOTHROW;
+    /// Deconstructor
+    virtual ~file_not_found_exception() MAPTK_NOTHROW;
+
+    /// The path that could not be found
+    path_t m_file_path;
+};
+
+class MAPTK_CORE_EXPORT file_not_read_exception
+  : public config_io_exception
+{
+  public:
+    /**
+     * \brief Constructor
+     * \param file_path The file path on which the read was attempted.
+     * \param reason    The reason for the read exception.
+     */
+    file_not_read_exception(path_t const& file_path, char const* reason) MAPTK_NOTHROW;
+    /// Deconstructor
+    virtual ~file_not_read_exception() MAPTK_NOTHROW;
+
+    /// The path to the file on which the read was attempted
+    path_t m_file_path;
+};
+
+class MAPTK_CORE_EXPORT file_write_exception
+  : public config_io_exception
+{
+  public:
+    /**
+     * \bried Constructor
+     * \param file_path The file path to which the write was attempted.
+     * \param reason    The reason for the write exception
+     */
+    file_write_exception(path_t const& file_path, char const* reason) MAPTK_NOTHROW;
+    /// Deconstructor
+    virtual ~file_write_exception() MAPTK_NOTHROW;
+
+    /// The path to the file on which the write was attempted.
+    path_t m_file_path;
+};
+
+}
+
+#endif // MAPTK_CORE_EXCEPTIONS_CONFIG_IO_H
