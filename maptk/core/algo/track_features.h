6c08174278e3c3c589e9c6803871e86bac3bb2ef
Matt Leotta <matt.leotta@kitware.com>
2013-12-13 16:36:14 -0500

    add initial tracking implementation

--- maptk/core/algo/track_features.h
+++ maptk/core/algo/track_features.h
@@ -8,6 +8,9 @@
 #define MAPTK_ALGO_TRACK_FEATURES_H_
 
 #include <maptk/core/algo/algorithm.h>
+#include <maptk/core/algo/detect_features.h>
+#include <maptk/core/algo/extract_descriptors.h>
+#include <maptk/core/algo/match_features.h>
 #include <maptk/core/image_container.h>
 #include <maptk/core/track_set.h>
 
@@ -36,7 +39,63 @@ public:
         unsigned int frame_number,
         image_container_sptr image_data) const = 0;
 
+  /// Set the feature detection algorithm to use in tracking
+  void set_feature_detector(detect_features_sptr alg)
+  {
+    detector_ = alg;
+  }
+
+  /// Set the descriptor extraction algorithm to use in tracking
+  void set_descriptor_extractor(extract_descriptors_sptr alg)
+  {
+    extractor_ = alg;
+  }
+
+  /// Set the feature matching algorithm to use in tracking
+  void set_feature_matcher(match_features_sptr alg)
+  {
+    matcher_ = alg;
+  }
+
+protected:
+  /// The feature detector algorithm to use
+  detect_features_sptr detector_;
+
+  /// The descriptor extractor algorithm to use
+  extract_descriptors_sptr extractor_;
+
+  /// The feature matching algorithm to use
+  match_features_sptr matcher_;
 };
+
+
+/// A basic feature tracker
+class simple_track_features
+: public algo::algorithm_impl<simple_track_features, track_features>
+{
+public:
+  /// Default Constructor
+  simple_track_features() {}
+
+  /// Copy Constructor
+  simple_track_features(const simple_track_features&) {}
+
+  /// Return the name of this implementation
+  std::string impl_name() const { return "simple"; }
+
+  /// Extend a previous set of tracks using the current frame
+  /**
+   * \param [in] prev_tracks the tracks from previous tracking steps
+   * \param [in] frame_number the frame number of the current frame
+   * \param [in] image_data the image pixels for the current frame
+   * \returns an updated set a tracks including the current frame
+   */
+  virtual track_set_sptr
+  track(track_set_sptr prev_tracks,
+        unsigned int frame_number,
+        image_container_sptr image_data) const;
+};
+
 
 } // end namespace algo
 
