8251c9ee7c09e83c8650d5980d818b152a514b71
Matt Leotta <matt.leotta@kitware.com>
2013-12-19 12:58:24 -0500

    add abstract estimate homography algorithm

---
+++ maptk/core/algo/estimate_homography.cxx
@@ -0,0 +1,11 @@
+/*ckwg +5
+ * Copyright 2013 by Kitware, Inc. All Rights Reserved. Please refer to
+ * KITWARE_LICENSE.TXT for licensing information, or contact General Counsel,
+ * Kitware, Inc., 28 Corporate Drive, Clifton Park, NY 12065.
+ */
+
+#include <maptk/core/algo/estimate_homography.h>
+#include <maptk/core/algo/algorithm.txx>
+#include <boost/foreach.hpp>
+
+INSTANTIATE_ALGORITHM_DEF(maptk::algo::estimate_homography);
---
+++ maptk/core/algo/estimate_homography.h
@@ -0,0 +1,55 @@
+/*ckwg +5
+ * Copyright 2013 by Kitware, Inc. All Rights Reserved. Please refer to
+ * KITWARE_LICENSE.TXT for licensing information, or contact General Counsel,
+ * Kitware, Inc., 28 Corporate Drive, Clifton Park, NY 12065.
+ */
+
+#ifndef MAPTK_ALGO_ESTIMATE_HOMOGRAPHY_H_
+#define MAPTK_ALGO_ESTIMATE_HOMOGRAPHY_H_
+
+#include <vector>
+
+#include <maptk/core/algo/algorithm.h>
+#include <maptk/core/vector.h>
+#include <maptk/core/matrix.h>
+#include <boost/shared_ptr.hpp>
+
+namespace maptk
+{
+
+namespace algo
+{
+
+/// An abstract base class for estimating a homography from matching 2D points
+class estimate_homography : public algorithm_def<estimate_homography>
+{
+public:
+  /// Return the name of this algorithm
+  std::string type_name() const { return "estimate_homography"; }
+
+  /// Estimate a homography matrix from corresponding points
+  /**
+   * \param [in]  pts1 the vector or corresponding points from the source image
+   * \param [in]  pts2 the vector of corresponding points from the destination image
+   * \param [out] inliers for each point pair, the value is true if
+   *                      this pair is an inlier to the homography estimate
+   * \param [in]  inlier_scale error distance tolerated for matches to be inliers
+   */
+  virtual matrix_3x3d
+  estimate(const std::vector<vector_2d>& pts1,
+           const std::vector<vector_2d>& pts2,
+           std::vector<bool>& inliers,
+           double inlier_scale = 1.0) const = 0;
+
+};
+
+
+typedef boost::shared_ptr<estimate_homography> estimate_homography_sptr;
+
+
+} // end namespace algo
+
+} // end namespace maptk
+
+
+#endif // MAPTK_ALGO_ESTIMATE_HOMOGRAPHY_H_
