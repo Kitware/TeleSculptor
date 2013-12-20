13e1cd298080b0b1c743ed0434201fd4f47df7ff
Matt Leotta <matt.leotta@kitware.com>
2013-12-20 17:08:15 -0500

    more sophisticated OpenCV estimate homography tests

--- tests/ocv/test_estimate_homography.cxx
+++ tests/ocv/test_estimate_homography.cxx
@@ -8,8 +8,27 @@
 
 #include <maptk/ocv/register.h>
 #include <maptk/ocv/estimate_homography.h>
+#include <opencv2/core/core.hpp>
 
 
+maptk::vector_2d random_point(double mean, double stdev)
+{
+  std::vector<double> data(2);
+  cv::randn(data, mean, stdev);
+  return maptk::vector_2d(data[0], data[1]);
+}
+
+
+maptk::matrix_3x3d sample_homography()
+{
+  maptk::matrix_3x3d H(0.0);
+  H(0,0) = 2.0;
+  H(1,1) = 3,0;
+  H(2,2) = 1.0;
+  H(0,2) = -1.5;
+  H(1,2) = 5.0;
+  return H;
+}
 
 #define TEST_ARGS ()
 
@@ -82,12 +101,7 @@ IMPLEMENT_TEST(four_points)
   pts1.push_back(vector_2d(0.0, 2.0));
   pts1.push_back(vector_2d(2.0, 2.0));
 
-  matrix_3x3d true_H(0.0);
-  true_H(0,0) = 2.0;
-  true_H(1,1) = 3,0;
-  true_H(2,2) = 1.0;
-  true_H(0,2) = -1.5;
-  true_H(1,2) = 5.0;
+  matrix_3x3d true_H = sample_homography();
 
   // transform pts1 to pts2 using true_H
   for(unsigned i=0; i<pts1.size(); ++i)
@@ -97,20 +111,133 @@ IMPLEMENT_TEST(four_points)
   }
 
   matrix_3x3d H = estimator.estimate(pts1, pts2, inliers);
+  H /= H(2,2);
+
+  double H_error = (true_H - H).frobenius_norm();
+  std::cout << "Homography estimation error: "<< H_error << std::endl;
 
   std::cout << "H = "<<true_H <<std::endl;
   TEST_NEAR("Frobenius norm between estimated and true homography",
-            (true_H - H).frobenius_norm(), 0.0, 1e-14);
+            H_error, 0.0, 1e-14);
 }
 
 
-IMPLEMENT_TEST(apply_to_points)
+IMPLEMENT_TEST(ideal_points)
 {
   using namespace maptk;
   ocv::estimate_homography estimator;
 
-  std::vector<vector_2d> pts1, pts2;
-  std::vector<bool> inliers;
+  matrix_3x3d true_H = sample_homography();
 
+  // create random points that perfectly correspond via true_H
+  std::vector<vector_2d> pts1, pts2;
+  for(unsigned i=0; i<100; ++i)
+  {
+    vector_2d v2 = random_point(500.0, 1000.0);
+    pts1.push_back(v2);
+    vector_3d v3 = true_H * vector_3d(v2.x(), v2.y(), 1.0);
+    pts2.push_back(vector_2d(v3.x()/v3.z(), v3.y()/v3.z()));
+  }
+
+  std::vector<bool> inliers;
   matrix_3x3d H = estimator.estimate(pts1, pts2, inliers);
+  H /= H(2,2);
+
+  double H_error = (true_H - H).frobenius_norm();
+  std::cout << "Homography estimation error: "<< H_error << std::endl;
+  TEST_NEAR("Frobenius norm between estimated and true homography",
+            H_error, 0.0, 1e-4);
+
+  unsigned num_inliers = std::count(inliers.begin(), inliers.end(), true);
+  std::cout << "num inliers "<<num_inliers<<std::endl;
+  TEST_EQUAL("All points are inliers", num_inliers, 100);
+}
+
+
+IMPLEMENT_TEST(noisy_points)
+{
+  using namespace maptk;
+  ocv::estimate_homography estimator;
+
+  matrix_3x3d true_H = sample_homography();
+
+  // create random points + noise that approximately correspond via true_H
+  std::vector<vector_2d> pts1, pts2;
+  for(unsigned i=0; i<100; ++i)
+  {
+    vector_2d v2 = random_point(500.0, 1000.0);
+    pts1.push_back(v2 + random_point(0, 0.1));
+    vector_3d v3 = true_H * vector_3d(v2.x(), v2.y(), 1.0);
+    pts2.push_back(vector_2d(v3.x()/v3.z(), v3.y()/v3.z())
+                   + random_point(0, 0.1));
+  }
+
+  std::vector<bool> inliers;
+  matrix_3x3d H = estimator.estimate(pts1, pts2, inliers);
+  H /= H(2,2);
+
+  double H_error = (true_H - H).frobenius_norm();
+  std::cout << "Homography estimation error: "<< H_error << std::endl;
+  TEST_NEAR("Frobenius norm between estimated and true homography",
+            H_error, 0.0, 0.1);
+
+  unsigned num_inliers = std::count(inliers.begin(), inliers.end(), true);
+  std::cout << "num inliers "<<num_inliers<<std::endl;
+  if (num_inliers < 90)
+  {
+    TEST_ERROR("Fewer than 90% of points detected as inliers");
+  }
+}
+
+
+IMPLEMENT_TEST(outlier_points)
+{
+  using namespace maptk;
+  ocv::estimate_homography estimator;
+
+  matrix_3x3d true_H = sample_homography();
+
+  // create random points + noise that approximately correspond via true_H
+  std::vector<vector_2d> pts1, pts2;
+  std::vector<bool> true_inliers;
+  for(unsigned i=0; i<100; ++i)
+  {
+    vector_2d v2 = random_point(500.0, 1000.0);
+    pts1.push_back(v2);
+    vector_3d v3 = true_H * vector_3d(v2.x(), v2.y(), 1.0);
+    pts2.push_back(vector_2d(v3.x()/v3.z(), v3.y()/v3.z()));
+    true_inliers.push_back(true);
+    if (i%3 == 0)
+    {
+      pts2.back() = random_point(500.0, 1000.0);
+      true_inliers.back() = false;
+    }
+  }
+
+  std::vector<bool> inliers;
+  matrix_3x3d H = estimator.estimate(pts1, pts2, inliers);
+  H /= H(2,2);
+
+  double H_error = (true_H - H).frobenius_norm();
+  std::cout << "Homography estimation error: "<< H_error << std::endl;
+  TEST_NEAR("Frobenius norm between estimated and true homography",
+            H_error, 0.0, 1e-4);
+
+  unsigned num_inliers = std::count(inliers.begin(), inliers.end(), true);
+  std::cout << "num inliers "<<num_inliers<<std::endl;
+
+  unsigned correct_inliers = 0;
+  for( unsigned i=0; i<inliers.size(); ++i )
+  {
+    if (true_inliers[i] == inliers[i])
+    {
+      ++correct_inliers;
+    }
+  }
+  std::cout << "correct inliers "<<correct_inliers<<std::endl;
+
+  if (correct_inliers < 90)
+  {
+    TEST_ERROR("Fewer than 90% of points have correct inlier status");
+  }
 }
