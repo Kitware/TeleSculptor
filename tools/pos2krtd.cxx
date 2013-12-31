a529473601d698abae59142339edd2b6c416115f
Matt Leotta <matt.leotta@kitware.com>
2013-12-31 12:26:34 -0500

    Refactor tool to allow processing all POS files in a directory.
    Now depends on Boost filesystem in order to iterate over directories.

--- tools/pos2krtd.cxx
+++ tools/pos2krtd.cxx
@@ -13,6 +13,70 @@
 #include <maptk/modules.h>
 #include <maptk/core/local_geo_cs.h>
 
+#include <boost/filesystem.hpp>
+#include <boost/foreach.hpp>
+
+namespace fs = boost::filesystem;
+
+
+/// Convert a POS file to a KRTD file
+bool convert_pos2krtd(const std::string& pos_filename,
+                      const std::string& krtd_filename,
+                      maptk::local_geo_cs& cs,
+                      maptk::camera_d base_camera)
+{
+  std::ifstream ifs(pos_filename.c_str());
+  if (!ifs)
+  {
+    std::cerr << "Error: Could not open POS file "<<pos_filename<<std::endl;
+    return false;
+  }
+
+  std::ofstream ofs(krtd_filename.c_str());
+  if (!ofs)
+  {
+    std::cerr << "Error: Could not open KRTD file "<<krtd_filename<<std::endl;
+    return false;
+  }
+
+  maptk::ins_data ins;
+  ifs >> ins;
+
+  if( cs.utm_origin_zone() < 0 )
+  {
+    std::cout << "lat: "<<ins.lat<<" lon: "<<ins.lon<<std::endl;
+    cs.set_utm_origin_zone(cs.geo_map_algo()->latlon_zone(ins.lat, ins.lon));
+    std::cout << "using zone "<< cs.utm_origin_zone() <<std::endl;
+  }
+
+  cs.update_camera(ins, base_camera);
+
+  ofs << base_camera;
+
+  return true;
+}
+
+
+/// Convert a directory of POS file to a directory of KRTD files
+bool convert_pos2krtd_dir(const fs::path& pos_dir,
+                          const fs::path& krtd_dir,
+                          maptk::local_geo_cs& cs,
+                          maptk::camera_d base_camera)
+{
+  fs::directory_iterator it(pos_dir), eod;
+  BOOST_FOREACH(fs::path const &p, std::make_pair(it, eod))
+  {
+    fs::path krtd_filename = krtd_dir / (basename(p) + ".krtd");
+    std::cout << "processing "<< p <<" --> " << krtd_filename<< std::endl;
+    if( !convert_pos2krtd(p.string(), krtd_filename.string(), cs, base_camera) )
+    {
+      return false;
+    }
+  }
+  return true;
+}
+
+
 static int maptk_main(int argc, char const* argv[])
 {
   // register the algorithms in the various modules for dynamic look-up
@@ -24,23 +88,6 @@ static int maptk_main(int argc, char const* argv[])
     return EXIT_FAILURE;
   }
 
-  std::ifstream ifs(argv[1]);
-  if (!ifs)
-  {
-    std::cerr << "Error: Could not open POS file "<<argv[1]<<std::endl;
-    return EXIT_FAILURE;
-  }
-
-  std::ofstream ofs(argv[2]);
-  if (!ofs)
-  {
-    std::cerr << "Error: Could not open KRTD file "<<argv[2]<<std::endl;
-    return EXIT_FAILURE;
-  }
-
-  maptk::ins_data ins;
-  ifs >> ins;
-
   maptk::algo::geo_map_sptr geo_mapper = maptk::algo::geo_map::create("proj");
   if( !geo_mapper )
   {
@@ -49,14 +96,33 @@ static int maptk_main(int argc, char const* argv[])
   }
 
   maptk::local_geo_cs local_cs(geo_mapper);
-  std::cout << "lat: "<<ins.lat<<" lon: "<<ins.lon<<std::endl;
-  local_cs.set_utm_origin_zone(geo_mapper->latlon_zone(ins.lat, ins.lon));
-  std::cout << "using zone "<< local_cs.utm_origin_zone() <<std::endl;
 
-  maptk::camera_d cam;
-  local_cs.update_camera(ins, cam);
-
-  ofs << cam;
+  if( fs::is_directory(argv[1]) )
+  {
+    std::cout << "processing "<<argv[1]<<" as a directory of POS files" << std::endl;
+    if( !fs::exists(argv[2]) )
+    {
+      if( !fs::create_directory(argv[2]) )
+      {
+        std::cerr << "Unable to create output directory: " << argv[2] << std::endl;
+        return EXIT_FAILURE;
+      }
+    }
+    else if( !fs::is_directory(argv[2]) )
+    {
+      std::cerr << argv[2] <<" exists but is not a directory." << std::endl;
+      return EXIT_FAILURE;
+    }
+    if( !convert_pos2krtd_dir(argv[1], argv[2], local_cs, maptk::camera_d()) )
+    {
+      return EXIT_FAILURE;
+    }
+  }
+  // otherwise treat input as a single file
+  else if( !convert_pos2krtd(argv[1], argv[2], local_cs, maptk::camera_d()) )
+  {
+    return EXIT_FAILURE;
+  }
 
   return EXIT_SUCCESS;
 }
