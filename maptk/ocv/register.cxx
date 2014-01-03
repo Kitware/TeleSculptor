fc371d76b3a61b5296c1325b1e315213e5a89626
Matt Leotta <matt.leotta@kitware.com>
2014-01-03 17:48:11 -0500

    need to register the non-free module to use SURF

--- maptk/ocv/register.cxx
+++ maptk/ocv/register.cxx
@@ -1,5 +1,5 @@
 /*ckwg +5
- * Copyright 2013 by Kitware, Inc. All Rights Reserved. Please refer to
+ * Copyright 2013-2014 by Kitware, Inc. All Rights Reserved. Please refer to
  * KITWARE_LICENSE.TXT for licensing information, or contact General Counsel,
  * Kitware, Inc., 28 Corporate Drive, Clifton Park, NY 12065.
  */
@@ -10,6 +10,7 @@
 #include <maptk/ocv/estimate_homography.h>
 #include <maptk/ocv/extract_descriptors.h>
 #include <maptk/ocv/match_features.h>
+#include <opencv2/nonfree/nonfree.hpp>
 
 namespace maptk
 {
@@ -20,6 +21,7 @@ namespace ocv
 /// register all algorithms in this module
 void register_algorithms()
 {
+  cv::initModule_nonfree();
   ocv::image_io::register_self();
   ocv::detect_features::register_self();
   ocv::estimate_homography::register_self();
