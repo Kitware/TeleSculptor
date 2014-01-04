c0dd31732cf34a1ff40569374f7ed83f49fe29eb
Matt Leotta <matt.leotta@kitware.com>
2014-01-04 09:26:26 -0500

    added missing 'ckwg +5' line

--- scripts/draw_tracks.py
+++ scripts/draw_tracks.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 """
-Copyright 2013 by Kitware, Inc. All Rights Reserved. Please refer to
+ckwg +5
+Copyright 2013-2014 by Kitware, Inc. All Rights Reserved. Please refer to
 KITWARE_LICENSE.TXT for licensing information, or contact General Counsel,
 Kitware, Inc., 28 Corporate Drive, Clifton Park, NY 12065.
 
