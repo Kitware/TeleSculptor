ef72559ca545fc61410f8d8cf956b023003464b8
Paul Tunison <paul.tunison@kitware.com>
2014-01-13 18:17:20 -0500

    Add flags structures to CMake

---
+++ cmake/maptk-flags-gnu.cmake
@@ -0,0 +1,5 @@
+#
+# Compiler flags specific to use with GCC
+#
+
+maptk_check_compiler_flag(-fvisibility=hidden)
---
+++ cmake/maptk-flags.cmake
@@ -0,0 +1,29 @@
+#
+# Encapsulation of flags that need to be set for MAPTK under different
+# circumstances.
+#
+
+include(CheckCXXCompilerFlag)
+
+define_property(GLOBAL PROPERTY maptk_warnings
+  BRIEF_DOCS "Warning flags for MAPTK build"
+  FULL_DOCS "List of warning flags MAPTK will build with."
+  )
+
+# Helper function for adding compiler flags
+function(maptk_check_compiler_flag flag)
+  string(REPLACE "+" "plus" safeflag "${flag}")
+  check_cxx_compiler_flag("${flag}" "has_compiler_flag-${safeflag}")
+  if ("has_compiler_flag-${safeflag}")
+    set_property(GLOBAL APPEND PROPERTY maptk_warnings "${flag}")
+  endif()
+endfunction ()
+
+if (MSVC)
+  include(maptk-flags-msvc)
+else()
+  include(maptk-flags-gnu)
+endif()
+
+get_property(maptk_cxx_flags GLOBAL PROPERTY maptk_warnings)
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${maptk_cxx_flags}")
