b84e504b460a9a8586d5350ddac5f2d9e1c2aa55
Matt Leotta <matt.leotta@kitware.com>
2014-01-21 17:35:20 -0500

    start a test for vxl::bundle_adjust

---
+++ tests/vxl/test_bundle_adjust.cxx
@@ -0,0 +1,118 @@
+/*ckwg +5
+ * Copyright 2014 by Kitware, Inc. All Rights Reserved. Please refer to
+ * KITWARE_LICENSE.TXT for licensing information, or contact General Counsel,
+ * Kitware, Inc., 28 Corporate Drive, Clifton Park, NY 12065.
+ */
+
+#include <test_common.h>
+
+#include <maptk/vxl/register.h>
+#include <maptk/vxl/bundle_adjust.h>
+#include <vnl/vnl_random.h>
+
+static vnl_random rng;
+
+
+maptk::vector_2d random_point(double mean, double stdev)
+{
+  maptk::vector_2d v(rng.normal64(), rng.normal64());
+  return stdev * v + mean;
+}
+
+#define TEST_ARGS ()
+
+DECLARE_TEST_MAP();
+
+int
+main(int argc, char* argv[])
+{
+  CHECK_ARGS(1);
+
+  maptk::vxl::register_algorithms();
+
+  testname_t const testname = argv[1];
+
+  RUN_TEST(testname);
+}
+
+
+IMPLEMENT_TEST(create)
+{
+  using namespace maptk;
+  algo::bundle_adjust_sptr ba = algo::bundle_adjust::create("vxl");
+  if (!ba)
+  {
+    TEST_ERROR("Unable to create vxl::bundle_adjust by name");
+  }
+}
+
+
+IMPLEMENT_TEST(cube)
+{
+  using namespace maptk;
+  vxl::bundle_adjust ba;
+
+  // create landmarks at the corners of a cube
+  std::vector<vector_3d> landmark_pts;
+  landmark_pts.push_back(vector_3d(-1, -1, -1));
+  landmark_pts.push_back(vector_3d(-1, -1,  1));
+  landmark_pts.push_back(vector_3d(-1,  1, -1));
+  landmark_pts.push_back(vector_3d(-1,  1,  1));
+  landmark_pts.push_back(vector_3d( 1, -1, -1));
+  landmark_pts.push_back(vector_3d( 1, -1,  1));
+  landmark_pts.push_back(vector_3d( 1,  1, -1));
+  landmark_pts.push_back(vector_3d( 1,  1,  1));
+  const size_t num_pts = landmark_pts.size();
+
+  // create a camera sequence (elliptical path)
+  // and a the projections of each landmark into each frame
+  camera_intrinsics_d K(1000, vector_2d(640,480));
+  rotation_d R; // identity
+  std::map<unsigned int, camera_sptr> cameras;
+  std::vector<std::vector<vector_2d> > projections(landmark_pts.size());
+  const size_t num_cams = 20;
+  for (size_t i=0; i<num_cams; ++i)
+  {
+    double frac = static_cast<double>(i) / num_cams;
+    double x = 4 * std::cos(2*frac);
+    double y = 3 * std::sin(2*frac);
+    camera_d* cam = new camera_d(vector_3d(x,y,2+frac), R, K);
+    // look at the origin
+    cam->look_at(vector_3d(0,0,0));
+    cameras[i] = camera_sptr(cam);
+    //std::cout << "camera "<<i<<" center "<<cam->get_center()<<std::endl;
+    for (size_t j=0; j<num_pts; ++j)
+    {
+      projections[j].push_back(cam->project(landmark_pts[j]));
+      //std::cout << "camera "<<i<<" point "<<j<<" : "<<projections[j].back() <<std::endl;
+    }
+  }
+
+  // create tracks from the projections
+  std::vector<track_sptr> tracks;
+  for (size_t i=0; i<num_pts; ++i)
+  {
+    track_sptr t(new track);
+    t->set_id(i);
+    tracks.push_back(t);
+    for (size_t j=0; j<num_cams; ++j)
+    {
+      feature_sptr f(new feature_d(projections[i][j]));
+      t->append(track::track_state(j, f, descriptor_sptr()));
+    }
+  }
+
+  // create landmarks from landmark_pts
+  std::map<unsigned long, landmark_sptr> landmarks;
+  for (size_t i=0; i<num_pts; ++i)
+  {
+    //landmarks[i] = landmark_sptr(new landmark_d(landmark_pts[i]));
+    landmarks[i] = landmark_sptr(new landmark_d(vector_3d(0,0,0)));
+  }
+
+  camera_map_sptr cam_map(new simple_camera_map(cameras));
+  landmark_map_sptr lm_map(new simple_landmark_map(landmarks));
+  track_set_sptr trk_set(new simple_track_set(tracks));
+  ba.optimize(cam_map, lm_map, trk_set);
+
+}
