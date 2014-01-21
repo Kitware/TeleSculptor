9579f46b09c95e841b34a8633745db3a1bf4f31f
Matt Leotta <matt.leotta@kitware.com>
2014-01-21 14:39:20 -0500

    register vxl::bundle_adjust for dynamic creation

--- maptk/vxl/register.cxx
+++ maptk/vxl/register.cxx
@@ -1,10 +1,12 @@
 /*ckwg +5
- * Copyright 2013 by Kitware, Inc. All Rights Reserved. Please refer to
+ * Copyright 2013-2014 by Kitware, Inc. All Rights Reserved. Please refer to
  * KITWARE_LICENSE.TXT for licensing information, or contact General Counsel,
  * Kitware, Inc., 28 Corporate Drive, Clifton Park, NY 12065.
  */
 
 #include <maptk/vxl/register.h>
+
+#include <maptk/vxl/bundle_adjust.h>
 #include <maptk/vxl/estimate_homography.h>
 #include <maptk/vxl/image_io.h>
 
@@ -17,6 +19,7 @@ namespace vxl
 /// register all algorithms in this module
 void register_algorithms()
 {
+  vxl::bundle_adjust::register_self();
   vxl::estimate_homography::register_self();
   vxl::image_io::register_self();
 }
