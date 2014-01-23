f3286845ae232e31384f104a43b094309dc903a6
Matt Leotta <matt.leotta@kitware.com>
2014-01-23 18:03:19 -0500

    added tests for Rodrigues and axis/angle rotation representations

--- tests/core/test_rotation.cxx
+++ tests/core/test_rotation.cxx
@@ -1,5 +1,5 @@
 /*ckwg +5
- * Copyright 2013 by Kitware, Inc. All Rights Reserved. Please refer to
+ * Copyright 2013-2014 by Kitware, Inc. All Rights Reserved. Please refer to
  * KITWARE_LICENSE.TXT for licensing information, or contact General Counsel,
  * Kitware, Inc., 28 Corporate Drive, Clifton Park, NY 12065.
  */
@@ -45,4 +45,44 @@ IMPLEMENT_TEST(convert_matrix)
     TEST_ERROR("Default rotation is not identity matrix");
   }
 
+}
+
+
+IMPLEMENT_TEST(convert_rodrigues)
+{
+  using namespace maptk;
+
+  rotation_d rot;
+  TEST_EQUAL("Rodrigues vector of identity is zero",
+              rot.rodrigues().magnitude(), 0.0);
+
+  rotation_d rot2(vector_3d(0,0,0));
+  TEST_EQUAL("Rotation from zero Rodrigues vector is identity",
+              rotation_d(vector_3d(0,0,0)),
+              rotation_d());
+
+  vector_3d rvec(2,-1,0.5);
+  rotation_d rot3(rvec);
+  TEST_NEAR("Rodrigues convert to and from rotation is same",
+            (rot3.rodrigues() - rvec).magnitude(), 0.0, 1e-14);
+
+  TEST_NEAR("Rodrigues direction == rotation axis",
+            (normalized(rvec) - rot3.axis()).magnitude(), 0.0, 1e-14);
+}
+
+
+IMPLEMENT_TEST(convert_axis_angle)
+{
+  using namespace maptk;
+
+  double angle = 0.8;
+  vector_3d axis = normalized(vector_3d(-3,2,1));
+
+  rotation_d rot(angle, axis);
+
+  TEST_NEAR("Angle extracted from rotation matches input",
+            rot.angle(), angle, 1e-14);
+
+  TEST_NEAR("Axis extracted from rotation matches input",
+            (rot.axis() - axis).magnitude(), 0.0, 1e-14);
 }
