b6558a80a478387a4636efb32a67d25ce1a2e3c2
Paul Tunison <paul.tunison@kitware.com>
2014-02-05 12:54:23 -0500

    fixed copyright years

--- maptk/ocv/match_features.cxx
+++ maptk/ocv/match_features.cxx
@@ -1,5 +1,5 @@
 /*ckwg +5
- * Copyright 2013 by Kitware, Inc. All Rights Reserved. Please refer to
+ * Copyright 2013-2014 by Kitware, Inc. All Rights Reserved. Please refer to
  * KITWARE_LICENSE.TXT for licensing information, or contact General Counsel,
  * Kitware, Inc., 28 Corporate Drive, Clifton Park, NY 12065.
  */
