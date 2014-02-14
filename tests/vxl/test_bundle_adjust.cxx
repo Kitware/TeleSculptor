45b8aeba0a40b717163cfd8a28ea8b79c78d0e72
Matt Leotta <matt.leotta@kitware.com>
2014-02-14 16:42:24 -0500

    Moved common helper functions for tests into shared header files.

--- tests/vxl/test_bundle_adjust.cxx
+++ tests/vxl/test_bundle_adjust.cxx
@@ -5,29 +5,13 @@
  */
 
 #include <test_common.h>
+#include <test_scene.h>
 
 #include <maptk/core/metrics.h>
 #include <maptk/vxl/register.h>
 #include <maptk/vxl/bundle_adjust.h>
-#include <vnl/vnl_random.h>
 #include <boost/foreach.hpp>
 
-static vnl_random rng;
-
-
-inline
-maptk::vector_3d random_point3(double stdev)
-{
-  maptk::vector_3d v(rng.normal64(), rng.normal64(), rng.normal64());
-  return stdev * v;
-}
-
-inline
-maptk::vector_2d random_point2(double stdev)
-{
-  maptk::vector_2d v(rng.normal64(), rng.normal64());
-  return stdev * v;
-}
 
 #define TEST_ARGS ()
 
@@ -39,7 +23,6 @@ main(int argc, char* argv[])
   CHECK_ARGS(1);
 
   maptk::vxl::register_algorithms();
-  rng.reseed(1234);
 
   testname_t const testname = argv[1];
 
@@ -55,212 +38,6 @@ IMPLEMENT_TEST(create)
   {
     TEST_ERROR("Unable to create vxl::bundle_adjust by name");
   }
-}
-
-
-// construct a map of landmarks at the corners of a cube centered at c
-// with a side length of s
-maptk::landmark_map_sptr
-cube_corners(double s, const maptk::vector_3d& c=maptk::vector_3d(0,0,0))
-{
-  using namespace maptk;
-
-  // create corners of a cube
-  landmark_map::map_landmark_t landmarks;
-  s /= 2.0;
-  landmarks[0] = landmark_sptr(new landmark_d(c + vector_3d(-s, -s, -s)));
-  landmarks[1] = landmark_sptr(new landmark_d(c + vector_3d(-s, -s,  s)));
-  landmarks[2] = landmark_sptr(new landmark_d(c + vector_3d(-s,  s, -s)));
-  landmarks[3] = landmark_sptr(new landmark_d(c + vector_3d(-s,  s,  s)));
-  landmarks[4] = landmark_sptr(new landmark_d(c + vector_3d( s, -s, -s)));
-  landmarks[5] = landmark_sptr(new landmark_d(c + vector_3d( s, -s,  s)));
-  landmarks[6] = landmark_sptr(new landmark_d(c + vector_3d( s,  s, -s)));
-  landmarks[7] = landmark_sptr(new landmark_d(c + vector_3d( s,  s,  s)));
-
-  return landmark_map_sptr(new simple_landmark_map(landmarks));
-}
-
-
-// construct map of landmarks will all locations at c
-maptk::landmark_map_sptr
-init_landmarks(maptk::landmark_id_t num_lm,
-               const maptk::vector_3d& c=maptk::vector_3d(0,0,0))
-{
-  using namespace maptk;
-
-  landmark_map::map_landmark_t lm_map;
-  for (landmark_id_t i=0; i<num_lm; ++i)
-  {
-    lm_map[i] = landmark_sptr(new landmark_d(c));
-  }
-  return landmark_map_sptr(new simple_landmark_map(lm_map));
-}
-
-
-// add Gaussian noise to the landmark positions
-maptk::landmark_map_sptr
-noisy_landmarks(maptk::landmark_map_sptr landmarks,
-                double stdev=1.0)
-{
-  using namespace maptk;
-
-  landmark_map::map_landmark_t lm_map = landmarks->landmarks();
-  BOOST_FOREACH(landmark_map::map_landmark_t::value_type& p, lm_map)
-  {
-    landmark_d& lm = dynamic_cast<landmark_d&>(*p.second);
-    lm.set_loc(lm.get_loc() + random_point3(stdev));
-  }
-  return landmark_map_sptr(new simple_landmark_map(lm_map));
-}
-
-
-// create a camera sequence (elliptical path)
-maptk::camera_map_sptr
-camera_seq(maptk::frame_id_t num_cams = 20)
-{
-  using namespace maptk;
-  camera_map::map_camera_t cameras;
-
-  // create a camera sequence (elliptical path)
-  camera_intrinsics_d K(1000, vector_2d(640,480));
-  rotation_d R; // identity
-  for (frame_id_t i=0; i<num_cams; ++i)
-  {
-    double frac = static_cast<double>(i) / num_cams;
-    double x = 4 * std::cos(2*frac);
-    double y = 3 * std::sin(2*frac);
-    camera_d* cam = new camera_d(vector_3d(x,y,2+frac), R, K);
-    // look at the origin
-    cam->look_at(vector_3d(0,0,0));
-    cameras[i] = camera_sptr(cam);
-  }
-  return camera_map_sptr(new simple_camera_map(cameras));
-}
-
-
-// create an initial camera sequence with all cameras at the same location
-maptk::camera_map_sptr
-init_cameras(maptk::frame_id_t num_cams = 20)
-{
-  using namespace maptk;
-  camera_map::map_camera_t cameras;
-
-  // create a camera sequence (elliptical path)
-  camera_intrinsics_d K(1000, vector_2d(640,480));
-  rotation_d R; // identity
-  vector_3d c(0, 0, 1);
-  for (frame_id_t i=0; i<num_cams; ++i)
-  {
-    camera_d* cam = new camera_d(c, R, K);
-    // look at the origin
-    cam->look_at(vector_3d(0,0,0), vector_3d(0,1,0));
-    cameras[i] = camera_sptr(cam);
-  }
-  return camera_map_sptr(new simple_camera_map(cameras));
-}
-
-
-// add positional and rotational Gaussian noise to cameras
-maptk::camera_map_sptr
-noisy_cameras(maptk::camera_map_sptr cameras,
-              double pos_stdev=1.0, double rot_stdev=1.0)
-{
-  using namespace maptk;
-
-  camera_map::map_camera_t cam_map = cameras->cameras();
-  BOOST_FOREACH(camera_map::map_camera_t::value_type& p, cam_map)
-  {
-    camera_d& cam = dynamic_cast<camera_d&>(*p.second);
-    cam.set_center(cam.get_center() + random_point3(pos_stdev));
-    rotation_d rand_rot(random_point3(rot_stdev));
-    cam.set_rotation(cam.get_rotation() * rand_rot);
-  }
-  return camera_map_sptr(new simple_camera_map(cam_map));
-}
-
-
-// create tracks by projecting the landmarks into the cameras
-maptk::track_set_sptr
-projected_tracks(maptk::landmark_map_sptr landmarks,
-                 maptk::camera_map_sptr cameras)
-{
-  using namespace maptk;
-  std::vector<track_sptr> tracks;
-  camera_map::map_camera_t cam_map = cameras->cameras();
-  landmark_map::map_landmark_t lm_map = landmarks->landmarks();
-  const track_id_t num_pts = static_cast<track_id_t>(landmarks->size());
-  for (track_id_t i=0; i<num_pts; ++i)
-  {
-    track_sptr t(new track);
-    t->set_id(i);
-    tracks.push_back(t);
-    BOOST_FOREACH(const camera_map::map_camera_t::value_type& p, cam_map)
-    {
-      const camera_d& cam = dynamic_cast<const camera_d&>(*p.second);
-      feature_sptr f(new feature_d(cam.project(lm_map[i]->loc())));
-      t->append(track::track_state(p.first, f, descriptor_sptr()));
-    }
-  }
-  return track_set_sptr(new simple_track_set(tracks));
-}
-
-
-// randomly drop a fraction of the track states
-maptk::track_set_sptr
-subset_tracks(maptk::track_set_sptr in_tracks, double keep_frac=0.75)
-{
-  using namespace maptk;
-
-  std::srand(0);
-  std::vector<track_sptr> tracks = in_tracks->tracks();
-  std::vector<track_sptr> new_tracks;
-  const int rand_thresh = static_cast<int>(keep_frac * RAND_MAX);
-  BOOST_FOREACH(const track_sptr& t, tracks)
-  {
-    track_sptr nt(new track);
-    nt->set_id(t->id());
-    std::cout << "track "<<t->id()<<":";
-    for(track::history_const_itr it=t->begin(); it!=t->end(); ++it)
-    {
-      if(std::rand() < rand_thresh)
-      {
-        nt->append(*it);
-        std::cout << " .";
-      }
-      else
-      {
-        std::cout << " X";
-      }
-    }
-    std::cout << std::endl;
-    new_tracks.push_back(nt);
-  }
-  return track_set_sptr(new simple_track_set(new_tracks));
-}
-
-
-// add Gaussian noise to track feature locations
-maptk::track_set_sptr
-noisy_tracks(maptk::track_set_sptr in_tracks, double stdev=1.0)
-{
-  using namespace maptk;
-
-  std::vector<track_sptr> tracks = in_tracks->tracks();
-  std::vector<track_sptr> new_tracks;
-  BOOST_FOREACH(const track_sptr& t, tracks)
-  {
-    track_sptr nt(new track);
-    nt->set_id(t->id());
-    for(track::history_const_itr it=t->begin(); it!=t->end(); ++it)
-    {
-      vector_2d loc = it->feat->loc() + random_point2(stdev);
-      track::track_state ts(*it);
-      ts.feat = feature_sptr(new feature_d(loc));
-      nt->append(ts);
-    }
-    new_tracks.push_back(nt);
-  }
-  return track_set_sptr(new simple_track_set(new_tracks));
 }
 
 
--- tests/vxl/test_estimate_homography.cxx
+++ tests/vxl/test_estimate_homography.cxx
@@ -5,19 +5,10 @@
  */
 
 #include <test_common.h>
+#include <test_random_point.h>
 
 #include <maptk/vxl/register.h>
 #include <maptk/vxl/estimate_homography.h>
-#include <vnl/vnl_random.h>
-
-static vnl_random rng;
-
-
-maptk::vector_2d random_point(double mean, double stdev)
-{
-  maptk::vector_2d v(rng.normal64(), rng.normal64());
-  return stdev * v + mean;
-}
 
 
 maptk::matrix_3x3d sample_homography()
@@ -133,7 +124,7 @@ IMPLEMENT_TEST(ideal_points)
   std::vector<vector_2d> pts1, pts2;
   for(unsigned i=0; i<100; ++i)
   {
-    vector_2d v2 = random_point(500.0, 1000.0);
+    vector_2d v2 = random_point2(1000.0) + 500.0;
     pts1.push_back(v2);
     vector_3d v3 = true_H * vector_3d(v2.x(), v2.y(), 1.0);
     pts2.push_back(vector_2d(v3.x()/v3.z(), v3.y()/v3.z()));
@@ -166,11 +157,11 @@ IMPLEMENT_TEST(noisy_points)
   std::vector<vector_2d> pts1, pts2;
   for(unsigned i=0; i<100; ++i)
   {
-    vector_2d v2 = random_point(500.0, 1000.0);
-    pts1.push_back(v2 + random_point(0, 0.1));
+    vector_2d v2 = random_point2(1000.0) + 500.0;
+    pts1.push_back(v2 + random_point2(0.1));
     vector_3d v3 = true_H * vector_3d(v2.x(), v2.y(), 1.0);
     pts2.push_back(vector_2d(v3.x()/v3.z(), v3.y()/v3.z())
-                   + random_point(0, 0.1));
+                   + random_point2(0.1));
   }
 
   std::vector<bool> inliers;
@@ -204,14 +195,14 @@ IMPLEMENT_TEST(outlier_points)
   std::vector<bool> true_inliers;
   for(unsigned i=0; i<100; ++i)
   {
-    vector_2d v2 = random_point(500.0, 1000.0);
+    vector_2d v2 = random_point2(1000.0) + 500.0;
     pts1.push_back(v2);
     vector_3d v3 = true_H * vector_3d(v2.x(), v2.y(), 1.0);
     pts2.push_back(vector_2d(v3.x()/v3.z(), v3.y()/v3.z()));
     true_inliers.push_back(true);
     if (i%3 == 0)
     {
-      pts2.back() = random_point(500.0, 1000.0);
+      pts2.back() = random_point2(1000.0) + 500.0;
       true_inliers.back() = false;
     }
   }
