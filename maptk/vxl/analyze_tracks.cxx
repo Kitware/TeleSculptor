2796793a3e51d042a20beb47c95da584b0cfa282
Matt Dawkins <matt.dawkins@kitware.com>
2014-02-26 17:37:49 -0500

    Add simple output

--- maptk/vxl/analyze_tracks.cxx
+++ maptk/vxl/analyze_tracks.cxx
@@ -13,10 +13,12 @@
 #include <maptk/vxl/analyze_tracks.h>
 
 #include <boost/foreach.hpp>
+#include <boost/lexical_cast.hpp>
 
 #include <set>
 #include <fstream>
-
+#include <iostream>
+#include <iomanip>
 
 namespace maptk
 {
@@ -32,12 +34,16 @@ public:
 
   /// Constructor
   priv()
+  : output_sum_props(true),
+    output_pt_matrix(true),
+    pt_matrix_cols(5)
   {
   }
 
   /// Copy Constructor
   priv(const priv& other)
   {
+    *this = other;
   }
 
   /// Destructor
@@ -45,7 +51,10 @@ public:
   {
   }
 
-  /// parameters - none yet
+  /// Parameters
+  bool output_sum_props;
+  bool output_pt_matrix;
+  unsigned pt_matrix_cols;
 };
 
 
@@ -77,7 +86,6 @@ config_block_sptr
 analyze_tracks
 ::get_configuration() const
 {
-  // get base config from base class
   config_block_sptr config = maptk::algo::analyze_tracks::get_configuration();
   return config;
 }
@@ -106,7 +114,50 @@ analyze_tracks
 ::print_info(track_set_sptr track_set,
              stream_t& stream) const
 {
+  const unsigned num_tracks = track_set->size();
+  const frame_id_t first_frame = track_set->first_frame();
+  const frame_id_t last_frame = track_set->last_frame();
 
+  // Output number of tracks in stream
+  if( d_->output_sum_props )
+  {
+    stream << "Track Set Properties" << std::endl;
+    stream << "--------------------" << std::endl;
+    stream << std::endl;
+    stream << "Largest Track ID: " << num_tracks << std::endl;
+    stream << "Smallest Frame ID: " << first_frame << std::endl;
+    stream << "Largest Frame ID: " << last_frame << std::endl;
+    stream << std::endl;
+  }
+
+  // Output percent tracked matrix
+  if( d_->output_pt_matrix )
+  {
+    stream << "Percent of Features Tracked Matrix" << std::endl;
+    stream << "----------------------------------" << std::endl;
+    stream << std::endl;
+
+    for( unsigned fid = first_frame; fid <= last_frame; fid++ )
+    {
+      stream << "Frame" << boost::lexical_cast<std::string>( fid );
+
+      for( unsigned c = 1; c <= d_->pt_matrix_cols; c++ )
+      {
+        stream << " ";
+
+        if( fid < first_frame + c )
+        {
+          stream << "----";
+        }
+        else
+        {
+          stream << std::setprecision(3) << track_set->percentage_tracked( fid-c, fid );
+        }
+      }
+
+      stream << std::endl;
+    }
+  }
 }
 
 
