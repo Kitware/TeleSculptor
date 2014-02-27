d1fbf8ee00c5bdd023f2d56d66b16451331f1c40
Matthew Dawkins <matt.dawkins@kitware.com>
2014-02-27 10:01:13 -0500

    Add new algo base classes for restructure

---
+++ maptk/core/algo/close_loops.h
@@ -0,0 +1,64 @@
+/*ckwg +5
+ * Copyright 2014 by Kitware, Inc. All Rights Reserved. Please refer to
+ * KITWARE_LICENSE.TXT for licensing information, or contact General Counsel,
+ * Kitware, Inc., 28 Corporate Drive, Clifton Park, NY 12065.
+ */
+
+#ifndef MAPTK_ALGO_CLOSE_LOOPS_H_
+#define MAPTK_ALGO_CLOSE_LOOPS_H_
+
+#include <maptk/core/core_config.h>
+
+#include <boost/shared_ptr.hpp>
+
+#include <maptk/core/algo/algorithm.h>
+#include <maptk/core/image_container.h>
+#include <maptk/core/track_set.h>
+
+#include <ostream>
+
+/**
+ * \file
+ * \brief Header defining abstract \link maptk::algo::close_loops track
+ *        analyzer \endlink algorithm
+ */
+
+namespace maptk
+{
+
+namespace algo
+{
+
+/// An abstract base class for algorithms which output various human readable
+/// statistics about track sets, to aid with both debugging and algorithm
+/// tuning (either manual or automatic).
+class MAPTK_CORE_EXPORT close_loops
+  : public algorithm_def<close_loops>
+{
+public:
+
+  typedef std::ostream stream_t;
+
+  /// Return the name of this algorithm.
+  std::string type_name() const { return "close_loops"; }
+
+  /// Attempt to find more track matches to aid in loop closure.
+  /**
+   * \param [in] frame_number the frame number of the current frame
+   * \param [in] input the input track set to stitch
+   * \returns an updated set a tracks after the stitching operation
+   */
+  virtual track_set_sptr
+  track_stitching( frame_id_t frame_number,
+                   track_set_sptr input ) const;
+
+};
+
+typedef boost::shared_ptr<close_loops> close_loops_sptr;
+
+} // end namespace algo
+
+} // end namespace maptk
+
+
+#endif // MAPTK_ALGO_CLOSE_LOOPS_H_
--- maptk/core/algo/draw_tracks.h
+++ maptk/core/algo/draw_tracks.h
@@ -4,8 +4,8 @@
  * Kitware, Inc., 28 Corporate Drive, Clifton Park, NY 12065.
  */
 
-#ifndef MAPTK_ALGO_ANALYZE_TRACKS_H_
-#define MAPTK_ALGO_ANALYZE_TRACKS_H_
+#ifndef MAPTK_ALGO_DRAW_TRACKS_H_
+#define MAPTK_ALGO_DRAW_TRACKS_H_
 
 #include <maptk/core/core_config.h>
 
@@ -19,8 +19,8 @@
 
 /**
  * \file
- * \brief Header defining abstract \link maptk::algo::analyze_tracks track
- *        analyzer \endlink algorithm
+ * \brief Header defining abstract \link maptk::algo::draw_tracks track
+ *        drawr \endlink algorithm
  */
 
 namespace maptk
@@ -32,41 +32,32 @@ namespace algo
 /// An abstract base class for algorithms which output various human readable
 /// statistics about track sets, to aid with both debugging and algorithm
 /// tuning (either manual or automatic).
-class MAPTK_CORE_EXPORT analyze_tracks
-  : public algorithm_def<analyze_tracks>
+class MAPTK_CORE_EXPORT draw_tracks
+  : public algorithm_def<draw_tracks>
 {
 public:
 
   typedef std::ostream stream_t;
 
   /// Return the name of this algorithm.
-  std::string type_name() const { return "analyze_tracks"; }
-
-  /// Output various information about the tracks stored in the input set.
-  /**
-   * \param [in] track_set the tracks to analyze
-   * \param [in] stream an output stream to write data onto
-   */
-  virtual void
-  print_info(track_set_sptr track_set,
-             stream_t& stream = std::cout) const = 0;
+  std::string type_name() const { return "draw_tracks"; }
 
   /// Write features tracks on top of the input images.
   /**
-   * \param [in] track_set the tracks to analyze
+   * \param [in] track_set the tracks to draw
    * \param [in] image_data a list of images the tracks were computed on
    */
   virtual void
-  write_images(track_set_sptr track_set,
-               image_container_sptr_list image_data) const = 0;
+  draw(track_set_sptr track_set,
+       image_container_sptr_list image_data) const = 0;
 
 };
 
-typedef boost::shared_ptr<analyze_tracks> analyze_tracks_sptr;
+typedef boost::shared_ptr<draw_tracks> draw_tracks_sptr;
 
 } // end namespace algo
 
 } // end namespace maptk
 
 
-#endif // MAPTK_ALGO_ANALYZE_TRACKS_H_
+#endif // MAPTK_ALGO_DRAW_TRACKS_H_
