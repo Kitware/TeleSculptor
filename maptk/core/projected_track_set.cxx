25c3362d0b6953ad1f82b00a3c088b02e689728d
Matt Dawkins <matt.dawkins@kitware.com>
2014-03-10 13:39:29 -0400

    Fix projected tracks func

--- maptk/core/projected_track_set.cxx
+++ maptk/core/projected_track_set.cxx
@@ -21,24 +21,25 @@ namespace maptk
 track_set_sptr
 projected_tracks(landmark_map_sptr landmarks, camera_map_sptr cameras)
 {
-  using namespace maptk;
   std::vector<track_sptr> tracks;
+
   camera_map::map_camera_t cam_map = cameras->cameras();
   landmark_map::map_landmark_t lm_map = landmarks->landmarks();
-  const track_id_t num_pts = static_cast<track_id_t>(landmarks->size());
-  for (track_id_t i=0; i<num_pts; ++i)
+
+  for( landmark_map::map_landmark_t::iterator l = lm_map.begin(); l != lm_map.end(); l++ )
   {
-    track_sptr t(new track);
-    t->set_id(i);
-    tracks.push_back(t);
-    BOOST_FOREACH(const camera_map::map_camera_t::value_type& p, cam_map)
+    track_sptr t( new track );
+    t->set_id( l->first );
+    tracks.push_back( t );
+
+    BOOST_FOREACH( const camera_map::map_camera_t::value_type& p, cam_map )
     {
-      const camera_d& cam = dynamic_cast<const camera_d&>(*p.second);
-      feature_sptr f(new feature_d(cam.project(lm_map[i]->loc())));
-      t->append(track::track_state(p.first, f, descriptor_sptr()));
+      const camera_d& cam = dynamic_cast<const camera_d&>( *p.second );
+      feature_sptr f( new feature_d( cam.project( l->second->loc() ) ) );
+      t->append( track::track_state( p.first, f, descriptor_sptr() ) );
     }
   }
-  return track_set_sptr(new simple_track_set(tracks));
+  return track_set_sptr( new simple_track_set( tracks ) );
 }
 
 
