054b8c05f7e5c28f9873b5c802417511ca25711f
Matt Dawkins <matt.dawkins@kitware.com>
2014-03-11 17:29:10 -0400

    Add homography class

---
+++ maptk/core/homography.cxx
@@ -0,0 +1,144 @@
+/*ckwg +5
+ * Copyright 2014 by Kitware, Inc. All Rights Reserved. Please refer to
+ * KITWARE_LICENSE.TXT for licensing information, or contact General Counsel,
+ * Kitware, Inc., 28 Corporate Drive, Clifton Park, NY 12065.
+ */
+
+/**
+ * \file
+ * \brief core homography related classes implementation
+ */
+
+#include "homography.h"
+
+namespace maptk
+{
+
+
+f2f_homography
+::f2f_homography( const homography& h,
+                  const frame_id_t from_id,
+                  const frame_id_t to_id )
+: homography(h),
+  from_id_(from_id),
+  to_id_(to_id)
+{
+}
+
+
+f2f_homography
+::~f2f_homography()
+{
+}
+
+
+f2f_homography
+f2f_homography
+::inverse() const
+{
+  return f2f_homography( maptk::inverse( *this ), to_id_, from_id_ );
+}
+
+
+frame_id_t
+f2f_homography
+::from_id() const
+{
+  return from_id_;
+}
+
+
+frame_id_t
+f2f_homography
+::to_id() const
+{
+  return to_id_;
+}
+
+
+homography_collection
+::homography_collection( f2f_homography_sptr cur_to_last,
+                         f2f_homography_sptr cur_to_ref,
+                         f2f_homography_sptr ref_to_wld )
+: current_to_last_( cur_to_last ),
+  current_to_reference_( cur_to_ref ),
+  reference_to_world_( ref_to_wld )
+{
+}
+
+
+homography_collection
+::~homography_collection()
+{
+}
+
+
+f2f_homography_sptr
+homography_collection
+::current_to_last() const
+{
+  return current_to_last_;
+}
+
+
+f2f_homography_sptr
+homography_collection
+::current_to_reference() const
+{
+  return current_to_reference_;
+}
+
+
+f2f_homography_sptr
+homography_collection
+::reference_to_world() const
+{
+  return reference_to_world_;
+}
+
+
+bool
+homography_collection
+::has_current_to_last() const
+{
+  return current_to_last_;
+}
+
+bool
+homography_collection
+::has_current_to_reference() const
+{
+  return current_to_reference_;
+}
+
+bool
+homography_collection
+::has_reference_to_world() const
+{
+  return reference_to_world_;
+}
+
+
+void
+homography_collection
+::set_current_to_last( f2f_homography_sptr h )
+{
+  current_to_last_ = h;
+}
+
+void
+homography_collection
+::set_current_to_reference( f2f_homography_sptr h )
+{
+  current_to_reference_ = h;
+}
+
+void
+homography_collection
+::set_reference_to_world( f2f_homography_sptr h )
+{
+  reference_to_world_ = h;
+}
+
+
+} // end namespace maptk
---
+++ maptk/core/homography.h
@@ -0,0 +1,143 @@
+/*ckwg +5
+ * Copyright 2014 by Kitware, Inc. All Rights Reserved. Please refer to
+ * KITWARE_LICENSE.TXT for licensing information, or contact General Counsel,
+ * Kitware, Inc., 28 Corporate Drive, Clifton Park, NY 12065.
+ */
+
+/**
+ * \file
+ * \brief core homography related classes
+ */
+
+#ifndef MAPTK_HOMOGRAPHY_H_
+#define MAPTK_HOMOGRAPHY_H_
+
+#include "core_config.h"
+#include "matrix.h"
+
+#include <iostream>
+#include <map>
+#include <vector>
+
+#include <boost/shared_ptr.hpp>
+
+namespace maptk
+{
+
+
+/// A raw homography transformation.
+typedef matrix_3x3d homography;
+
+
+/// A smart pointer to a raw homography.
+typedef boost::shared_ptr< homography > homography_sptr;
+
+
+/// A homography between two arbitrary frames.
+class f2f_homography : public homography
+{
+public:
+
+  /// Construct a frame to frame homography.
+  explicit f2f_homography( const homography& h,
+                           const frame_id_t from_id,
+                           const frame_id_t to_id );
+
+  /// Copy Constructor.
+  f2f_homography( const f2f_homography& h );
+
+  /// Destructor.
+  virtual ~f2f_homography();
+
+  /// Return the inverse of this homography.
+  virtual f2f_homography inverse() const;
+
+  /// The frame identifier that this homography maps from.
+  virtual frame_id_t from_id() const;
+
+  /// The frame identifier that this homography maps to.
+  virtual frame_id_t to_id() const;
+
+protected:
+
+  /// From frame identifier.
+  frame_id_t from_id_;
+
+  /// To frame identifier.
+  frame_id_t to_id_;
+
+};
+
+
+/// A smart pointer to a frame homography.
+typedef boost::shared_ptr< f2f_homography > f2f_homography_sptr;
+
+
+/// A set of frame to frame homographies for several individual frames.
+typedef std::map< frame_id_t, f2f_homography_sptr > f2f_homography_set;
+
+
+/// A smart pointer to a f2f homography set.
+typedef boost::shared_ptr< f2f_homography_set > f2f_homography_set_sptr;
+
+
+/// A collection of homography-related data for each individual frame.
+class homography_collection
+{
+public:
+
+  /// Construct a homography collection from different types of homographies.
+  homography_collection( f2f_homography_sptr cur_to_last = f2f_homography_sptr(),
+                         f2f_homography_sptr cur_to_ref = f2f_homography_sptr(),
+                         f2f_homography_sptr ref_to_wld = f2f_homography_sptr() );
+
+  /// Destructor.
+  virtual ~homography_collection();
+
+  /// Return a homography to the last frame.
+  f2f_homography_sptr current_to_last() const;
+  /// Return a homography to some reference frame.
+  f2f_homography_sptr current_to_reference() const;
+  /// Return an arbitrary reference to world homography.
+  f2f_homography_sptr reference_to_world() const;
+
+  /// Is the current to last homography valid?
+  bool has_current_to_last() const;
+  /// Is the current to reference homography valid?
+  bool has_current_to_reference() const;
+  /// Is the reference to world homography valid?
+  bool has_reference_to_world() const;
+
+  /// Set the current to last homography.
+  void set_current_to_last( f2f_homography_sptr h );
+  /// Set the current to reference homography.
+  void set_current_to_reference( f2f_homography_sptr h );
+  /// Set the current to world homography.
+  void set_reference_to_world( f2f_homography_sptr h );
+
+protected:
+
+  /// The actual current to last homography.
+  f2f_homography_sptr current_to_last_;
+  /// The actual current to reference homography.
+  f2f_homography_sptr current_to_reference_;
+  /// The actual reference to world homography.
+  f2f_homography_sptr reference_to_world_;
+};
+
+
+/// A pointer to a homography collection class.
+typedef boost::shared_ptr< homography_collection> homography_collection_sptr;
+
+
+/// A set of homography collections for several individual frames.
+typedef std::map< frame_id_t, homography_collection > homography_collection_set;
+
+
+/// A pointer to a homography_collection_set.
+typedef boost::shared_ptr< homography_collection_set > homography_collection_set_sptr;
+
+
+} // end namespace maptk
+
+#endif // MAPTK_HOMOGRAPHY_H_
