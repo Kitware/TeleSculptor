8fa60bd481d41d3ae806cf0f8b7babdf7425fc14
Matt Dawkins <matt.dawkins@kitware.com>
2014-03-12 16:13:07 -0400

    Add algorithm to register

--- maptk/core/algo/register.cxx
+++ maptk/core/algo/register.cxx
@@ -10,12 +10,13 @@
  */
 
 #include <maptk/core/algo/register.h>
+
 #include <maptk/core/algo/close_loops_bad_frames_only.h>
 #include <maptk/core/algo/convert_image.h>
 #include <maptk/core/algo/hierarchical_bundle_adjust.h>
 #include <maptk/core/algo/match_features_homography.h>
 #include <maptk/core/algo/track_features_default.h>
-
+#include <maptk/core/algo/map_groundplane_default.h>
 
 namespace maptk
 {
@@ -29,6 +30,7 @@ void register_algorithms()
   close_loops_bad_frames_only::register_self();
   convert_image_default::register_self();
   hierarchical_bundle_adjust::register_self();
+  map_groundplane_default::register_self();
   match_features_homography::register_self();
   track_features_default::register_self();
 }
