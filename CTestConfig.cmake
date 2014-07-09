e303915270528d8be4722c525a0a868548d25d99
Paul Tunison <paul.tunison@kitware.com>
2014-07-09 12:20:54 -0400

    Updated CTest config to encapsulate both internal and public  configurations

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -7,7 +7,14 @@
 set(CTEST_PROJECT_NAME "MAPTK")
 set(CTEST_NIGHTLY_START_TIME "01:00:00 UTC")
 
-set(CTEST_DROP_METHOD "http")
-set(CTEST_DROP_SITE "open.cdash.org")
-set(CTEST_DROP_LOCATION "/submit.php?project=MAPTK")
+if (NOT MAPTK_INTERNAL_DASHBOARD)
+  set(CTEST_DROP_METHOD "http")
+  set(CTEST_DROP_SITE "open.cdash.org")
+  set(CTEST_DROP_LOCATION "/submit.php?project=MAPTK")
+else()
+  set(CTEST_DROP_METHOD "https")
+  set(CTEST_DROP_SITE "www.kitware.com/CDash")
+  set(CTEST_DROP_LOCATION "/submit.php?project=maptk-private")
+endif()
+
 set(CTEST_DROP_SITE_CDASH TRUE)
