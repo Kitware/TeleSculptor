746da915acc17c42b805b85ca328d508a538df15
Paul Tunison <paul.tunison@kitware.com>
2014-12-15 11:44:19 -0500

    Added comments + added FindPROJ.cmake to installed CMake components

--- CMake/maptk-flags.cmake
+++ CMake/maptk-flags.cmake
@@ -5,6 +5,7 @@
 
 include(CheckCXXCompilerFlag)
 
+# Infrastructure for conveniently adding flags
 define_property(GLOBAL PROPERTY maptk_warnings
   BRIEF_DOCS "Warning flags for MAPTK build"
   FULL_DOCS "List of warning flags MAPTK will build with."
@@ -19,12 +20,14 @@ function(maptk_check_compiler_flag flag)
   endif()
 endfunction ()
 
+# Check for platform/compiler/IDE specific flags
 if (MSVC)
   include(maptk-flags-msvc)
 else()
   include(maptk-flags-gnu)
 endif()
 
+# Adding set flags via convenience structure to appropriate CMake property
 get_property(maptk_cxx_flags GLOBAL PROPERTY maptk_warnings)
 string(REPLACE ";" " " maptk_cxx_flags "${maptk_cxx_flags}")
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${maptk_cxx_flags}")
--- CMake/maptk-install-utils.cmake
+++ CMake/maptk-install-utils.cmake
@@ -9,6 +9,7 @@ set(utils_dir "${CMAKE_CURRENT_LIST_DIR}")
 
 install(
   FILES "${utils_dir}/maptk-utils.cmake"
+        "${utils_dir}/FindPROJ.cmake"
   DESTINATION "${maptk_cmake_install_dir}"
   )
 install(
