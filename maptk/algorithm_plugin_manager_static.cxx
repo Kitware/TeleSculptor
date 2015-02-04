b1f83aeaf6722fd41ac5402f8b463ab8cc58e371
Paul Tunison <paul.tunison@kitware.com>
2015-02-03 19:38:55 -0500

    Fixed header license comment funkiness

--- maptk/algorithm_plugin_manager_static.cxx
+++ maptk/algorithm_plugin_manager_static.cxx
@@ -10,13 +10,13 @@
  *
  *  * Redistributions in binary form must reproduce the above copyright notice,
  *    this list of conditions and the following disclaimer in the documentation
- *    [Maeand/or other materials provided with the distribution.
+ *    and/or other materials provided with the distribution.
  *
  *  * Neither name of Kitware, Inc. nor the names of any contributors may be used
- *    to e[Maendorse or promote products derived from this software without specific
+ *    to endorse or promote products derived from this software without specific
  *    prior written permission.
  *
- * THIS SOFTWA[M å[M#åRE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ``AS IS''
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ``AS IS''
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
  * ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHORS OR CONTRIBUTORS BE LIABLE FOR
--- maptk/algorithm_plugin_manager_static.h
+++ maptk/algorithm_plugin_manager_static.h
@@ -1,22 +1,22 @@
 /*ckwg +29
  * Copyright 2015 by Kitware, Inc.
  * All rights reserved.
- *e
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- *[Mae
+ *
  *  * Redistributions of source code must retain the above copyright notice,
  *    this list of conditions and the following disclaimer.
- *[Mae
+ *
  *  * Redistributions in binary form must reproduce the above copyright notice,
  *    this list of conditions and the following disclaimer in the documentation
- *    [Maeand/or other materials provided with the distribution.
+ *    and/or other materials provided with the distribution.
  *
  *  * Neither name of Kitware, Inc. nor the names of any contributors may be used
- *    to e[Maendorse or promote products derived from this software without specific
+ *    to endorse or promote products derived from this software without specific
  *    prior written permission.
  *
- * THIS SOFTWA[M å[M#åRE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ``AS IS''
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ``AS IS''
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
  * ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHORS OR CONTRIBUTORS BE LIABLE FOR
