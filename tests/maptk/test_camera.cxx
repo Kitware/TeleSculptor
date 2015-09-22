bd01bb3e22b4a83761d3ba855a193045f0f4d6f3
Matt Leotta <matt.leotta@kitware.com>
2015-09-22 13:35:51 -0400

    remove boost

--- tests/maptk/test_camera.cxx
+++ tests/maptk/test_camera.cxx
@@ -37,9 +37,16 @@
 
 #include <iostream>
 
-#include <boost/foreach.hpp>
-#include <boost/math/constants/constants.hpp>
+#define _USE_MATH_DEFINES
+#include <math.h>
 
+#if defined M_PIl
+#define LOCAL_PI M_PIl
+#else
+#define LOCAL_PI M_PI
+#endif
+
+#include <vital/vital_foreach.h>
 #include <maptk/camera.h>
 
 #define TEST_ARGS ()
@@ -66,7 +73,7 @@ IMPLEMENT_TEST(interpolation)
   using vital::rotation_d;
   using vital::simple_camera;
 
-  double pi = boost::math::constants::pi<double>();
+  const double pi = LOCAL_PI;
   simple_camera a(vector_3d(-1, -1, -1),
                   rotation_d(vector_4d(0, 0, 0, 1))),  // no rotation
                 b(vector_3d(3, 3, 3),
@@ -100,7 +107,7 @@ IMPLEMENT_TEST(multiple_interpolations)
   using vital::rotation_d;
   using vital::simple_camera;
 
-  double pi = boost::math::constants::pi<double>();
+  const double pi = LOCAL_PI;
   simple_camera a(vector_3d(-1, -1, -1),
                   rotation_d(vector_4d(0, 0, 0, 1))),        // no rotation
                 b(vector_3d(3, 3, 3),
@@ -113,7 +120,7 @@ IMPLEMENT_TEST(multiple_interpolations)
 
   cerr << "Vector size: " << cams.size() << endl;
   TEST_EQUAL("vector size", cams.size(), 5);
-  BOOST_FOREACH(simple_camera cam, cams)
+  VITAL_FOREACH(simple_camera cam, cams)
   {
     cerr << "\t" << cam.center() << " :: " << cam.rotation().axis() << " " << cam.rotation().angle() << endl;
   }
