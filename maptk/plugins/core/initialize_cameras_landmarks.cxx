53ee26ec35f096fb0225722c5eb1258148f302ac
Matt Leotta <matt.leotta@kitware.com>
2015-10-22 21:56:11 -0400

    Added a missing check for when no landmarks are seen.
    This fixes an infinite loop caused by the initialization continually
    trying to initialize frame zero because the pool of remaining frames
    is not empty, but it can't find any frames that observe the existing
    landmarks.

--- maptk/plugins/core/initialize_cameras_landmarks.cxx
+++ maptk/plugins/core/initialize_cameras_landmarks.cxx
@@ -763,6 +763,14 @@ next_best_frame(const track_set_sptr tracks,
       }
     }
   }
+
+  // check if remaining new frames see no existing landmarks
+  if( vis_count.empty() )
+  {
+    std::cout << "remaining frame do not see any existing landmarks" << std::endl;
+    return *new_frame_ids.begin();
+  }
+
   // find the maximum observation
   unsigned int max_count = 0;
   frame_id_t best_frame = 0;
