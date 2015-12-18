9769fd9b555f72b5ab91b34b32ce075e38e1eac3
Matt Leotta <matt.leotta@kitware.com>
2015-12-18 11:58:04 -0500

    Fixes for making an OS X App Bundle

---
+++ CMake/utils/maptk-utils-bundle.cmake
@@ -0,0 +1,29 @@
+#
+# MAPTK Bundle creation support
+#
+
+# Provide an override for gp_item_default_embedded_path function
+# in GetPrerequisites.cmake used by fixup_bundle.  This allows
+# MAP-Tk to copy libraries into lib instead of MacOS.
+function(gp_item_default_embedded_path_override
+         item default_embedded_path_var)
+
+  set(path "${default_embedded_path_var}")
+  set(overridden 0)
+  if(APPLE)
+    if(item MATCHES "\\.dylib$")
+      set(path "@executable_path/../lib")
+      set(overridden 1)
+    endif()
+    # Embed frameworks in the embedded "Frameworks" directory (sibling of MacOS):
+    #
+    if(NOT overridden)
+      if(item MATCHES "[^/]+\\.framework/")
+        set(path "@executable_path/../Frameworks")
+        set(overridden 1)
+      endif()
+    endif()
+  endif()
+
+  set(${default_embedded_path_var} "${path}" PARENT_SCOPE)
+endfunction()
