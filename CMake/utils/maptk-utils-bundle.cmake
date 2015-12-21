70ed58268e351aec2040da2a89bf08feb7e892a3
Matt Leotta <matt.leotta@kitware.com>
2015-12-21 11:07:58 -0500

    simplify gp_item_default_embedded_path_override

--- CMake/utils/maptk-utils-bundle.cmake
+++ CMake/utils/maptk-utils-bundle.cmake
@@ -8,22 +8,11 @@
 function(gp_item_default_embedded_path_override
          item default_embedded_path_var)
 
-  set(path "@executable_path")
-  set(overridden 0)
   if(APPLE)
     if(item MATCHES "\\.dylib$")
       set(path "@executable_path/../lib")
-      set(overridden 1)
-    endif()
-    # Embed frameworks in the embedded "Frameworks" directory (sibling of MacOS):
-    #
-    if(NOT overridden)
-      if(item MATCHES "[^/]+\\.framework/")
-        set(path "@executable_path/../Frameworks")
-        set(overridden 1)
-      endif()
+      set(${default_embedded_path_var} "${path}" PARENT_SCOPE)
     endif()
   endif()
 
-  set(${default_embedded_path_var} "${path}" PARENT_SCOPE)
 endfunction()
