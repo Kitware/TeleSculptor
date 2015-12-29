03aec0a9c7db7ff38bdc7686f9dd9724090ef87d
Matthew Woehlke <matthew.woehlke@kitware.com>
2015-12-29 14:24:56 -0500

    Additional gradients
    Add some additional gradients that may be useful for data visualization.
    (Having color choices is a good thing when trying to visualize against
    imagery. Also, the new Terrain is interesting for visualization of
    elevation.)

--- gui/GradientSelector.cxx
+++ gui/GradientSelector.cxx
@@ -47,6 +47,23 @@ qtGradient buildGradient(GradientSelector::Preset which)
 
   switch (which)
   {
+    case GradientSelector::Slate:
+      gradient.insertStop(0.00, QColor(0, 0, 0));
+      gradient.insertStop(0.25, QColor(56, 56, 76));
+      gradient.insertStop(0.37, QColor(84, 84, 112));
+      gradient.insertStop(0.50, QColor(112, 120, 144));
+      gradient.insertStop(0.75, QColor(172, 200, 200));
+      gradient.insertStop(1.00, QColor(255, 255, 255));
+      return gradient;
+
+    case GradientSelector::Wood:
+      gradient.insertStop(0.00, QColor(0, 0, 0));
+      gradient.insertStop(0.25, QColor(104, 64, 64));
+      gradient.insertStop(0.50, QColor(192, 128, 88));
+      gradient.insertStop(0.75, QColor(224, 208, 136));
+      gradient.insertStop(1.00, QColor(255, 255, 255));
+      return gradient;
+
     case GradientSelector::Royal:
       gradient.insertStop(0.00, QColor(0, 0, 0));
       gradient.insertStop(0.25, QColor(64, 32, 120));
@@ -90,6 +107,33 @@ qtGradient buildGradient(GradientSelector::Preset which)
       gradient.insertStop(1.00, QColor(255, 255, 216));
       return gradient;
 
+    case GradientSelector::Sky:
+      gradient.insertStop(0.00, QColor(8, 48, 108));
+      gradient.insertStop(0.25, QColor(32, 112, 180));
+      gradient.insertStop(0.50, QColor(104, 172, 212));
+      gradient.insertStop(0.75, QColor(196, 216, 236));
+      gradient.insertStop(1.00, QColor(255, 255, 255));
+      return gradient;
+
+    case GradientSelector::Hyacinth:
+      gradient.insertStop(0.00, QColor(76, 0, 76));
+      gradient.insertStop(0.15, QColor(120, 20, 120));
+      gradient.insertStop(0.25, QColor(136, 64, 156));
+      gradient.insertStop(0.50, QColor(140, 148, 196));
+      gradient.insertStop(0.75, QColor(188, 212, 228));
+      gradient.insertStop(1.00, QColor(255, 255, 255));
+      return gradient;
+
+    case GradientSelector::Lilac:
+      gradient.insertStop(0.00, QColor(104, 0, 40));
+      gradient.insertStop(0.25, QColor(204, 16, 96));
+      gradient.insertStop(0.33, QColor(216, 48, 132));
+      gradient.insertStop(0.50, QColor(216, 96, 168));
+      gradient.insertStop(0.62, QColor(200, 144, 196));
+      gradient.insertStop(0.75, QColor(212, 184, 216));
+      gradient.insertStop(1.00, QColor(255, 255, 255));
+      return gradient;
+
     case GradientSelector::Jet:
       gradient.insertStop(0.00, QColor(0, 0, 128));
       gradient.insertStop(0.15, QColor(0, 0, 255));
@@ -97,6 +141,20 @@ qtGradient buildGradient(GradientSelector::Preset which)
       gradient.insertStop(0.65, QColor(255, 255, 0));
       gradient.insertStop(0.85, QColor(255, 0, 0));
       gradient.insertStop(1.00, QColor(128, 0, 0));
+      return gradient;
+
+    case GradientSelector::Terrain:
+      gradient.insertStop(0.00, QColor(40, 40, 128));
+      gradient.insertStop(0.07, QColor(20, 80, 160));
+      gradient.insertStop(0.15, QColor(0, 120, 192));
+      gradient.insertStop(0.25, QColor(0, 184, 92));
+      gradient.insertStop(0.38, QColor(108, 220, 120));
+      gradient.insertStop(0.50, QColor(252, 255, 152));
+      gradient.insertStop(0.62, QColor(192, 160, 120));
+      gradient.insertStop(0.75, QColor(128, 92, 84));
+      gradient.insertStop(0.88, QColor(192, 164, 160));
+      gradient.insertStop(1.00, QColor(255, 255, 255));
+      gradient.setInterpolationMode(qtGradient::InterpolateCubic);
       return gradient;
 
     case GradientSelector::Parula:
@@ -143,14 +201,54 @@ qtGradient buildGradient(GradientSelector::Preset which)
       gradient.insertStop(1.00, QColor(255, 255, 255));
       return gradient;
 
+    case GradientSelector::Plasma:
+      gradient.insertStop(0.00, QColor(0, 0, 0));
+      gradient.insertStop(0.08, QColor(0, 16, 80));
+      gradient.insertStop(0.15, QColor(40, 0, 144));
+      gradient.insertStop(0.35, QColor(160, 0, 144));
+      gradient.insertStop(0.55, QColor(232, 60, 36));
+      gradient.insertStop(0.75, QColor(255, 232, 48));
+      gradient.insertStop(1.00, QColor(255, 255, 255));
+      return gradient;
+
     case GradientSelector::Blackbody:
-      gradient.insertStop(0.000, QColor(0, 0, 0));
-      gradient.insertStop(0.150, QColor(152, 24, 0));
-      gradient.insertStop(0.270, QColor(232, 44, 0));
-      gradient.insertStop(0.400, QColor(255, 176, 40));
-      gradient.insertStop(0.550, QColor(255, 232, 126));
-      gradient.insertStop(0.750, QColor(255, 255, 255));
-      gradient.insertStop(1.000, QColor(144, 172, 255));
+      gradient.insertStop(0.00, QColor(0, 0, 0));
+      gradient.insertStop(0.15, QColor(152, 24, 0));
+      gradient.insertStop(0.27, QColor(232, 44, 0));
+      gradient.insertStop(0.40, QColor(255, 176, 40));
+      gradient.insertStop(0.55, QColor(255, 232, 126));
+      gradient.insertStop(0.75, QColor(255, 255, 255));
+      gradient.insertStop(1.00, QColor(144, 172, 255));
+      return gradient;
+
+    case GradientSelector::Spring:
+      gradient.insertStop(0.00, Qt::yellow);
+      gradient.insertStop(1.00, Qt::cyan);
+      return gradient;
+
+    case GradientSelector::Tulip:
+      gradient.insertStop(0.00, Qt::magenta);
+      gradient.insertStop(1.00, Qt::yellow);
+      return gradient;
+
+    case GradientSelector::Moody:
+      gradient.insertStop(0.00, Qt::cyan);
+      gradient.insertStop(1.00, Qt::magenta);
+      return gradient;
+
+    case GradientSelector::Anaglyph:
+      gradient.insertStop(0.00, QColor(0, 96, 192));
+      gradient.insertStop(1.00, QColor(176, 0, 48));
+      return gradient;
+
+    case GradientSelector::Rainbow:
+      gradient.insertStop(0.0 / 6.0, Qt::red);
+      gradient.insertStop(1.0 / 6.0, Qt::yellow);
+      gradient.insertStop(2.0 / 6.0, Qt::green);
+      gradient.insertStop(3.0 / 6.0, Qt::cyan);
+      gradient.insertStop(4.0 / 6.0, Qt::blue);
+      gradient.insertStop(5.0 / 6.0, Qt::magenta);
+      gradient.insertStop(6.0 / 6.0, Qt::red);
       return gradient;
 
     default: // Gray
@@ -202,16 +300,28 @@ void addGradient(
 GradientSelector::GradientSelector(QWidget* parent) : QComboBox{parent}
 {
   addGradient(this, Gray, "Gray");
+  addGradient(this, Slate, "Slate");
+  addGradient(this, Wood, "Wood");
   addGradient(this, Royal, "Royal");
   addGradient(this, Ember, "Ember");
   addGradient(this, Ocean, "Ocean");
   addGradient(this, Sunset, "Sunset");
   addGradient(this, Carribean, "Carribean");
+  addGradient(this, Sky, "Sky");
+  addGradient(this, Hyacinth, "Hyacinth");
+  addGradient(this, Lilac, "Lilac");
   addGradient(this, Jet, "Jet");
+  addGradient(this, Terrain, "Terrain");
   addGradient(this, Parula, "Parula");
   addGradient(this, Viridis, "Viridis");
   addGradient(this, Earth, "Earth");
+  addGradient(this, Plasma, "Plasma");
   addGradient(this, Blackbody, "Blackbody");
+  addGradient(this, Spring, "Spring");
+  addGradient(this, Tulip, "Tulip");
+  addGradient(this, Moody, "Moody");
+  addGradient(this, Anaglyph, "Anaglyph");
+  addGradient(this, Rainbow, "Rainbow");
 }
 
 //-----------------------------------------------------------------------------
