44f040cc2c0a6ef9dce6ea61a1b3fd3609b1568c
Linus Sherrill <linus.sherrill@kitware.com>
2016-01-20 14:55:13 -0500

    Remove unneeded boost libraries.

--- CMake/maptk-depends-Boost.cmake
+++ CMake/maptk-depends-Boost.cmake
@@ -4,6 +4,6 @@ if(WIN32)
   set(Boost_USE_STATIC_LIBS TRUE)
 endif()
 
-find_package(Boost 1.50 REQUIRED COMPONENTS system filesystem program_options timer chrono)
+find_package(Boost 1.50 REQUIRED COMPONENTS system timer chrono)
 add_definitions(-DBOOST_ALL_NO_LIB)
 include_directories(SYSTEM ${Boost_INCLUDE_DIRS})
