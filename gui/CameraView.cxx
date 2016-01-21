ccef78535affa6334163d782c6ad5dec6f1174bb
Matthew Woehlke <matthew.woehlke@kitware.com>
2016-01-21 11:41:31 -0500

    "Simplify" update logic
    When the CameraView needs the scene repainted, only force a repaint of
    the render widget, not the entire encapsulating widget (which is
    superfluous). This is probably left over from when the CameraView
    directly subclassed QVTKWidget, rather than wrapping a whole UI around
    the render widget.

--- gui/CameraView.cxx
+++ gui/CameraView.cxx
@@ -549,7 +549,7 @@ void CameraView::setActiveFrame(unsigned frame)
   QTE_D();
 
   d->featureRep->SetActiveFrame(frame);
-  this->update();
+  d->UI.renderWidget->update();
 }
 
 //-----------------------------------------------------------------------------
@@ -615,7 +615,7 @@ void CameraView::addLandmark(
 
   d->landmarks.addPoint(x, y, 0.0, d->landmarkData.value(id));
 
-  this->update();
+  d->UI.renderWidget->update();
 }
 
 //-----------------------------------------------------------------------------
@@ -628,7 +628,7 @@ void CameraView::addResidual(
 
   d->residuals.addSegment(x1, y1, -0.2, x2, y2, -0.2);
 
-  this->update();
+  d->UI.renderWidget->update();
 }
 
 //-----------------------------------------------------------------------------
@@ -709,7 +709,7 @@ void CameraView::updateFeatures()
   if (d->featuresDirty)
   {
     d->featureRep->Update();
-    this->update();
+    d->UI.renderWidget->update();
 
     d->featuresDirty = false;
   }
