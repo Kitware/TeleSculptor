630ab08a10f5c5be2e7a1f5400b12c683ae3b559
Matthew Woehlke <matthew.woehlke@kitware.com>
2016-01-25 15:11:40 -0500

    Update copyrights
    Update the latest copyright year of files that have been modified in
    2016.

--- maptk/plugins/ocv/detect_features.h
+++ maptk/plugins/ocv/detect_features.h
@@ -1,5 +1,5 @@
 /*ckwg +29
- * Copyright 2013-2015 by Kitware, Inc.
+ * Copyright 2013-2016 by Kitware, Inc.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
