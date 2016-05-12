40b13b1903ec65ec1c76f67fb760cba2d5f268a4
Louis Amore <louis.amore@kitware.com>
2016-05-12 11:01:07 +0200

    fixed generation of depthmap surfaces from vti file

--- gui/Project.cxx
+++ gui/Project.cxx
@@ -150,6 +150,7 @@ bool Project::read(QString const& path)
     if (config->has_value("depthmaps_images_file"))
     {
       auto const& vtiFilePath = config->get_value<std::string>("depthmaps_images_file");
+      auto const& vtiBasePath =  QFileInfo(QString::fromStdString(vtiFilePath)).absoluteDir().absolutePath();
       QFile vtiFile(base.filePath(qtString(vtiFilePath)));
       if (vtiFile.open(QIODevice::ReadOnly | QIODevice::Text))
       {
@@ -159,9 +160,12 @@ bool Project::read(QString const& path)
         while (!vtiStream.atEnd())
         {
           vtiStream >> frameNum >> fileName;
+          std::string fileNamestr = vtiBasePath.toStdString() + QDir::separator().toAscii() +fileName.toStdString();
+          fileName = QString::fromStdString(fileNamestr);
           if (!DMvti.contains(frameNum))
           {
             DMvti.insert(frameNum,fileName);
+            std::cout << "filename = " << fileName.toStdString() <<std::endl;
           }
         }
       }
