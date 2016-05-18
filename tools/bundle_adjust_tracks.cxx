c0d4246b76544d3eb888dca14382a23fff8c8f16
Matt Leotta <matt.leotta@kitware.com>
2016-05-18 12:27:29 -0400

    refactored match matrix importance into it's own function

--- tools/bundle_adjust_tracks.cxx
+++ tools/bundle_adjust_tracks.cxx
@@ -337,57 +337,24 @@ kwiver::vital::track_set_sptr
 filter_tracks_importance(kwiver::vital::track_set_sptr tracks, double min_score)
 {
   using namespace kwiver;
+
+  // compute the match matrix
   std::vector<vital::frame_id_t> frames;
   Eigen::SparseMatrix<unsigned int> mm = maptk::match_matrix(tracks, frames);
 
-  // build a frame map for reverse lookup of matrix indices
-  std::map<vital::frame_id_t, unsigned int> frame_map;
-  const unsigned int num_frames = static_cast<unsigned int>(frames.size());
-  for( unsigned int i=0; i<num_frames; ++i )
-  {
-    frame_map[frames[i]] = i;
-  }
+  // compute the importance scores on the tracks
+  std::map<vital::track_id_t, double> importance =
+      maptk::match_matrix_track_importance(tracks, frames, mm);
 
-  // score the importance of each track against the match matrix
   std::vector<vital::track_sptr> trks = tracks->tracks();
-  std::vector<std::pair<double, vital::track_sptr> > scores;
-  VITAL_FOREACH(const vital::track_sptr& t, trks)
-  {
-    // get all the frames covered by this track
-    std::set<vital::frame_id_t> t_frames = t->all_frame_ids();
-    // map the frames to a vector of all valid matrix indices
-    std::set<unsigned int> t_ind;
-    VITAL_FOREACH(const vital::frame_id_t& fid, t_frames)
-    {
-      std::map<vital::frame_id_t, unsigned int>::const_iterator fmi = frame_map.find(fid);
-      // only add to the vector if in the map
-      if( fmi != frame_map.end() )
-      {
-        t_ind.insert(fmi->second);
-      }
-    }
-
-    // get the scores from the match matrix
-    double score = 0.0;
-    typedef std::set<unsigned int>::const_iterator sitr_t;
-    for( sitr_t tfi1 = t_ind.begin(); tfi1 != t_ind.end(); ++tfi1)
-    {
-      for( sitr_t tfi2 = tfi1; tfi2 != t_ind.end(); ++tfi2)
-      {
-        score += 1.0 / mm.coeffRef(*tfi2, *tfi1);
-      }
-    }
-    scores.push_back(std::make_pair(score, t));
-  }
-
-  //std::sort(scores.begin(), scores.end());
   std::vector<vital::track_sptr> good_trks;
-  VITAL_FOREACH(auto const& p, scores)
+  VITAL_FOREACH(kwiver::vital::track_sptr t, trks)
   {
-    //std::cout << "len " << p.second->size() << "\t score " << p.first << std::endl;
-    if( p.first >= min_score )
+    std::map<vital::track_id_t, double>::const_iterator itr;
+    if( (itr = importance.find(t->id())) != importance.end() &&
+        itr->second >= min_score)
     {
-      good_trks.push_back(p.second);
+      good_trks.push_back(t);
     }
   }
   return vital::track_set_sptr(new vital::simple_track_set(good_trks));
