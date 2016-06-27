8d45f1c4f51d286c1b542ce4e6131afdf93ded04
ALouis38 <louis.amore@kitware.com>
2016-06-27 14:28:13 +0200

    Replaced path.toStdString().c_str() with qPrintable(path)

--- gui/WorldView.cxx
+++ gui/WorldView.cxx
@@ -852,7 +852,7 @@ void WorldView::exportWebGLScene(QString const& path)
   }
 
   exporter->exportStaticScene(d->renderWindow->GetRenderers(), width, height,
-                              path.toStdString().c_str());
+                              qPrintable(path));
 
 }
 #endif
