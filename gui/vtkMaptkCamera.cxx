b60796018f44c374aeff9994d2d3c7f752650d52
ALouis38 <louis.amore@kitware.com>
2016-06-29 14:52:56 +0200

    Simplify and speed up unproject function

--- gui/vtkMaptkCamera.cxx
+++ gui/vtkMaptkCamera.cxx
@@ -1,32 +1,32 @@
 /*ckwg +29
- * Copyright 2016 by Kitware, Inc.
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- *  * Redistributions of source code must retain the above copyright notice,
- *    this list of conditions and the following disclaimer.
- *
- *  * Redistributions in binary form must reproduce the above copyright notice,
- *    this list of conditions and the following disclaimer in the documentation
- *    and/or other materials provided with the distribution.
- *
- *  * Neither the name Kitware, Inc. nor the names of any contributors may be
- *    used to endorse or promote products derived from this software without
- *    specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ``AS IS''
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHORS OR CONTRIBUTORS BE LIABLE FOR
- * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
+* Copyright 2015-2016 by Kitware, Inc.
+* All rights reserved.
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions are met:
+*
+*  * Redistributions of source code must retain the above copyright notice,
+*    this list of conditions and the following disclaimer.
+*
+*  * Redistributions in binary form must reproduce the above copyright notice,
+*    this list of conditions and the following disclaimer in the documentation
+*    and/or other materials provided with the distribution.
+*
+*  * Neither name of Kitware, Inc. nor the names of any contributors may be used
+*    to endorse or promote products derived from this software without specific
+*    prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ``AS IS''
+* AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+* IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+* ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHORS OR CONTRIBUTORS BE LIABLE FOR
+* ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+* DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+* SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+* CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+* OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+* OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
 
 #include "vtkMaptkCamera.h"
 
@@ -125,34 +125,15 @@ bool vtkMaptkCamera::UnprojectPoint(double pixel[2], double depth,
                                     kwiver::vital::vector_3d *unProjectedPoint)
 {
   // Build camera matrix
-  auto const K = this->MaptkCamera->intrinsics()->as_matrix();
+  auto const K = this->MaptkCamera->intrinsics()->as_matrix()
+                      .triangularView<Eigen::Upper>();
   auto const T = this->MaptkCamera->translation();
   auto const R = kwiver::vital::matrix_3x3d(this->MaptkCamera->rotation());
 
-  kwiver::vital::vector_4d homogenousPoint;
+  kwiver::vital::vector_3d homogenousPoint(pixel[0], pixel[1], 1.0);
+  homogenousPoint *= depth;
 
-  homogenousPoint(0) = pixel[0] * depth;
-  homogenousPoint(1) = pixel[1] * depth;
-  homogenousPoint(2) = depth;
-  homogenousPoint(3) = 1;
-  kwiver::vital::matrix_4x4d homogenousK;
-
-  homogenousK << K(0,0), K(0,1), K(0,2), 0,
-                 K(1,0), K(1,1), K(1,2), 0,
-                 K(2,0), K(2,1), K(2,2), 0,
-                 0,      0,      0,      1;
-
-  kwiver::vital::matrix_4x4d RT;
-  RT << R(0,0), R(0,1), R(0,2), T(0,0),
-        R(1,0), R(1,1), R(1,2), T(1,0),
-        R(2,0), R(2,1), R(2,2), T(2,0),
-        0,      0,      0,      1;
-
-  homogenousPoint = RT.inverse() * homogenousK.inverse() * homogenousPoint;
-
-  *unProjectedPoint << homogenousPoint(0)/homogenousPoint(3),
-                       homogenousPoint(1)/homogenousPoint(3),
-                       homogenousPoint(2)/homogenousPoint(3);
+  *unProjectedPoint = R.transpose() * ((K.solve(homogenousPoint)) - T);
 
   return true;
 }
