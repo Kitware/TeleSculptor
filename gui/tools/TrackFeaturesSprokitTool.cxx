4faba234a353a6b828f7399ea490e28b575471ef
Brian Clipp <brian.clipp@kitware.com>
2018-02-12 11:35:46 -0500

    Made kwiver search on the config path for the vocabulary file.

--- gui/tools/TrackFeaturesSprokitTool.cxx
+++ gui/tools/TrackFeaturesSprokitTool.cxx
@@ -38,6 +38,7 @@
 #include <vital/algo/image_io.h>
 #include <vital/algo/convert_image.h>
 #include <vital/algo/track_features.h>
+#include <vital/algo/video_input.h>
 
 #include <vital/config/config_block_io.h>
 #include <vital/types/metadata.h>
@@ -63,50 +64,46 @@ using kwiver::vital::algo::convert_image;
 using kwiver::vital::algo::convert_image_sptr;
 using kwiver::vital::algo::track_features;
 using kwiver::vital::algo::track_features_sptr;
+using kwiver::vital::algo::video_input;
+using kwiver::vital::algo::video_input_sptr;
 
 namespace
 {
-static char const* const BLOCK_IR = "image_reader";
-static char const* const BLOCK_CI = "image_converter";
-static char const* const BLOCK_TF = "feature_tracker";
+  static char const* const BLOCK_CI = "image_converter";
+  static char const* const BLOCK_TF = "feature_tracker";
+  static char const* const BLOCK_VR = "video_reader";
 
-//-----------------------------------------------------------------------------
-kwiver::vital::config_block_sptr readConfig(std::string const& name)
-{
-  try
+  //-----------------------------------------------------------------------------
+  kwiver::vital::config_block_sptr readConfig(std::string const& name)
   {
-    using kwiver::vital::read_config_file;
+    try
+    {
+      using kwiver::vital::read_config_file;
 
-    auto const exeDir = QDir(QApplication::applicationDirPath());
-    auto const prefix = stdString(exeDir.absoluteFilePath(".."));
-    return read_config_file(name, "maptk", MAPTK_VERSION, prefix);
+      auto const exeDir = QDir(QApplication::applicationDirPath());
+      auto const prefix = stdString(exeDir.absoluteFilePath(".."));
+      return read_config_file(name, "maptk", MAPTK_VERSION, prefix);
+    }
+    catch (...)
+    {
+      return{};
+    }
   }
-  catch (...)
-  {
-    return {};
-  }
-}
+}  // end anonymous namespace
 
-}
 
 //-----------------------------------------------------------------------------
 class TrackFeaturesSprokitToolPrivate
 {
 public:
   TrackFeaturesSprokitToolPrivate();
-  image_io_sptr image_reader;
   convert_image_sptr image_converter;
   track_features_sptr feature_tracker;
-  kwiver::vital::timestamp::frame_t m_frame_number;
-  kwiver::vital::timestamp::time_t m_frame_time;
-  kwiver::vital::timestamp::time_t m_config_frame_time;
+  video_input_sptr video_reader;
 };
 
 TrackFeaturesSprokitToolPrivate
-::TrackFeaturesSprokitToolPrivate():
-  m_frame_number(1),
-  m_frame_time(0),
-  m_config_frame_time(1000)
+::TrackFeaturesSprokitToolPrivate()
 {
 
 }
@@ -140,10 +137,10 @@ bool TrackFeaturesSprokitTool::execute(QWidget* window)
 {
   QTE_D();
 
-  if (!this->hasImagePaths())
+  if (!this->hasVideoSource())
   {
     QMessageBox::information(
-      window, "Insufficient data", "This operation requires Images.");
+      window, "Insufficient data", "This operation requires a video source.");
     return false;
   }
 
@@ -159,9 +156,8 @@ bool TrackFeaturesSprokitTool::execute(QWidget* window)
     return false;
   }
 
-  if (!image_io::check_nested_algo_configuration(BLOCK_IR, config) ||
-      !convert_image::check_nested_algo_configuration(BLOCK_CI, config))
-
+  if (!convert_image::check_nested_algo_configuration(BLOCK_CI, config) ||
+      !video_input::check_nested_algo_configuration(BLOCK_VR, this->data()->config))
   {
     QMessageBox::critical(
       window, "Configuration error",
@@ -170,23 +166,132 @@ bool TrackFeaturesSprokitTool::execute(QWidget* window)
   }
 
   // Create algorithm from configuration
-  image_io::set_nested_algo_configuration(BLOCK_IR, config, d->image_reader);
   convert_image::set_nested_algo_configuration(BLOCK_CI, config, d->image_converter);
+  video_input::set_nested_algo_configuration(BLOCK_VR, this->data()->config, d->video_reader);
 
   return AbstractTool::execute(window);
 }
 
+std::stringstream
+TrackFeaturesSprokitTool
+::create_pipeline_config()
+{
+  std::stringstream ss;
+
+  bool from_file = false;
+  if (from_file)
+  {
+    std::string pipe_file = "D:/export_controlled_data/telesculptor/sequence2/track_features_embedded.pipe";  //TODO THIS NEEDS TO POINT TO THE BUILD OR INSTALL DIR COPY
+
+                                                                                                              // Open pipeline description
+    std::ifstream pipe_str;
+    pipe_str.open(pipe_file, std::ifstream::in);
+
+    if (!pipe_str)
+    {
+      return ss;
+    }
+    ss << pipe_str.rdbuf();
+    return ss;
+  }
+  else
+  {
+    std::string voc_path;
+    kwiver::vital::config_path_list_t kwiver_config_path;
+    kwiversys::SystemTools::GetPath(kwiver_config_path, "KWIVER_CONFIG_PATH");
+    if (!kwiver_config_path.empty())
+    {
+      //look in the kwiver config directory
+      voc_path = kwiver_config_path[0] + "/kwiver_voc.yml.gz";
+    }
+    else
+    {
+      //look in the working directory
+      voc_path = "kwiver_voc.yml.gz";
+    }
+
+    ss << SPROKIT_PROCESS("input_adapter", "input")
+
+      << SPROKIT_PROCESS("feature_tracker", "tracker")
+      << SPROKIT_CONFIG("track_features:type", "ocv_KLT")
+      << SPROKIT_CONFIG("track_features:ocv_KLT:new_feat_exclusionary_radius_image_fraction", "0.02")
+      << SPROKIT_CONFIG("track_features:ocv_KLT:redetect_frac_lost_threshold", "0.7")
+      << SPROKIT_CONFIG("track_features:ocv_KLT:feature_detector:type", "ocv_FAST")
+      << SPROKIT_CONFIG("track_features:ocv_KLT:feature_detector:ocv_FAST:threshold", "50")
+      << SPROKIT_CONFIG("track_features:ocv_KLT:feature_detector:ocv_FAST:nonmaxSuppression", "true")
+
+      << SPROKIT_PROCESS("keyframe_selection_process", "keyframes")
+      << SPROKIT_CONFIG("keyframe_selection_1:type", "basic")
+      << SPROKIT_CONFIG("keyframe_selection_1:basic:fraction_tracks_lost_to_necessitate_new_keyframe", "0.1")
+
+      << SPROKIT_PROCESS("detect_features_if_keyframe_process", "detect_if_keyframe")
+      << SPROKIT_CONFIG("augment_keyframes:type","augment_keyframes")
+      << SPROKIT_CONFIG("augment_keyframes:augment_keyframes:kf_only_feature_detector:type", "ocv_ORB")
+      << SPROKIT_CONFIG("augment_keyframes:augment_keyframes:kf_only_descriptor_extractor:type", "ocv_ORB")
+      << SPROKIT_CONFIG("augment_keyframes:augment_keyframes:kf_only_feature_detector:ocv_ORB:n_features", "2000")
+
+      << SPROKIT_PROCESS("close_loops_process", "loop_detector")
+      << SPROKIT_CONFIG("close_loops:type", "appearance_indexed")
+      << SPROKIT_CONFIG("close_loops:appearance_indexed:min_loop_inlier_matches", "50")
+      << SPROKIT_CONFIG("close_loops:appearance_indexed:match_features:type", "homography_guided")
+      << SPROKIT_CONFIG("close_loops:appearance_indexed:match_features:homography_guided:homography_estimator:type", "ocv")
+      << SPROKIT_CONFIG("close_loops:appearance_indexed:match_features:homography_guided:feature_matcher1:type", "ocv_brute_force")
+      << SPROKIT_CONFIG("close_loops:appearance_indexed:bag_of_words_matching:type", "dbow2")
+      << SPROKIT_CONFIG("close_loops:appearance_indexed:bag_of_words_matching:dbow2:feature_detector:type", "ocv_ORB")
+      << SPROKIT_CONFIG("close_loops:appearance_indexed:bag_of_words_matching:dbow2:descriptor_extractor:type", "ocv_ORB")
+      << SPROKIT_CONFIG("close_loops:appearance_indexed:bag_of_words_matching:dbow2:image_io:type", "ocv")
+      << SPROKIT_CONFIG("close_loops:appearance_indexed:bag_of_words_matching:dbow2:max_num_candidate_matches_from_vocabulary_tree", "20")
+      << SPROKIT_CONFIG("close_loops:appearance_indexed:bag_of_words_matching:dbow2:training_image_list_path", "")
+      << SPROKIT_CONFIG("close_loops:appearance_indexed:bag_of_words_matching:dbow2:vocabulary_path", voc_path)
+
+      << SPROKIT_PROCESS("draw_tracks", "draw")
+      << SPROKIT_CONFIG("draw_tracks:type", "ocv")
+      << SPROKIT_CONFIG("draw_tracks:ocv:draw_track_ids", "false")
+      << SPROKIT_CONFIG("draw_tracks:ocv:draw_untracked_features", "true")
+      << SPROKIT_CONFIG("draw_tracks:ocv:draw_shift_lines", "false")
+      << SPROKIT_CONFIG("draw_tracks:ocv:draw_match_lines", "true")
+
+      << SPROKIT_PROCESS("image_viewer", "disp")
+      << SPROKIT_CONFIG("annotate_image", "true")
+      << SPROKIT_CONFIG("pause_time", "0.001")
+      << SPROKIT_CONFIG("title", "images")
+
+      << SPROKIT_PROCESS("output_adapter", "output")
+      << SPROKIT_CONFIG_BLOCK("_pipeline:_edge")
+      << SPROKIT_CONFIG("capacity", "2")
+
+      << SPROKIT_CONNECT("input", "image", "tracker", "image")
+      << SPROKIT_CONNECT("input", "timestamp", "tracker", "timestamp")
+      << SPROKIT_CONNECT("tracker", "feature_track_set", "tracker", "feature_track_set")
+      << SPROKIT_CONNECT("tracker", "feature_track_set", "keyframes", "next_tracks")
+      << SPROKIT_CONNECT("input", "timestamp", "keyframes", "timestamp")
+      << SPROKIT_CONNECT("keyframes", "feature_track_set", "keyframes", "loop_back_tracks")
+      << SPROKIT_CONNECT("keyframes", "feature_track_set", "detect_if_keyframe", "next_tracks")
+      << SPROKIT_CONNECT("detect_if_keyframe", "feature_track_set", "detect_if_keyframe", "loop_back_tracks")
+      << SPROKIT_CONNECT("input", "image", "detect_if_keyframe", "image")
+      << SPROKIT_CONNECT("input", "timestamp", "detect_if_keyframe", "timestamp")
+      << SPROKIT_CONNECT("detect_if_keyframe", "feature_track_set", "draw", "feature_track_set")
+      << SPROKIT_CONNECT("detect_if_keyframe", "feature_track_set", "loop_detector", "next_tracks")
+      << SPROKIT_CONNECT("loop_detector", "feature_track_set", "loop_detector", "loop_back_tracks")
+      << SPROKIT_CONNECT("input", "timestamp", "loop_detector", "timestamp")
+      << SPROKIT_CONNECT("input", "image", "draw", "image")
+      << SPROKIT_CONNECT("input", "timestamp", "disp", "timestamp")
+      << SPROKIT_CONNECT("draw", "output_image", "disp", "image")
+      << SPROKIT_CONNECT("loop_detector", "feature_track_set", "output", "feature_track_set");
+
+    return ss;
+  }
+}
+
 //-----------------------------------------------------------------------------
-void TrackFeaturesSprokitTool::run()
+void
+TrackFeaturesSprokitTool
+::run()
 {
   QTE_D();
 
-  std::string pipe_file = "D:/export_controlled_data/telesculptor/sequence2/track_features_embedded.pipe";  //TODO THIS NEEDS TO POINT TO THE BUILD OR INSTALL DIR COPY
-
-  // Open pipeline description
-  std::ifstream pipe_str;
-  pipe_str.open(pipe_file, std::ifstream::in);
-  if (!pipe_str)
+  std::stringstream pipe_str = create_pipeline_config();
+  if (pipe_str.str().empty())
   {
     return;
   }
@@ -199,28 +304,31 @@ void TrackFeaturesSprokitTool::run()
   ep.start();
 
   unsigned int frame = this->activeFrame();
-  auto const& paths = this->imagePaths();
+  kwiver::vital::timestamp currentTimestamp;
 
-  unsigned int i = frame;
-  for (; i < paths.size(); ++i)
+  d->video_reader->open(this->data()->videoPath);
+
+  // Seek to just before active frame TODO: check status?
+  if (frame > 1)
   {
-    auto const image = d->image_reader->load(paths[i]);
+    d->video_reader->seek_frame(currentTimestamp, frame - 1);
+  }
+
+  while (d->video_reader->next_frame(currentTimestamp))
+  {
+    auto const image = d->video_reader->frame_image();
     auto const converted_image = d->image_converter->convert(image);
-    // Set the metadata on the image.
-    // For now, the only metadata is the filename of the image.
-    auto md = std::make_shared<kwiver::vital::metadata>();
-    md->add(NEW_METADATA_ITEM(kwiver::vital::VITAL_META_IMAGE_FILENAME, paths[i]));
-    converted_image->set_metadata(md);
 
-    kwiver::vital::timestamp frame_ts(d->m_frame_time, d->m_frame_number);
-
-    ++d->m_frame_number;
-    d->m_frame_time += d->m_config_frame_time;
+    auto const mdv = d->video_reader->frame_metadata();
+    if (!mdv.empty())
+    {
+      converted_image->set_metadata(mdv[0]);
+    }
 
     // Create dataset for input
     auto ds = kwiver::adapter::adapter_data_set::create();
     ds->add_value("image", converted_image);
-    ds->add_value("timestamp", frame_ts);
+    ds->add_value("timestamp", currentTimestamp);
     ep.send(ds);
 
     if (this->isCanceled())
