2d47f3ad74cf531a85937873223fade7d03eed19
Eric Smith <eric.smith@khq.kitware.com>
2018-02-19 06:14:56 -0500

    added depth configs

--- gui/tools/ComputeDepthTool.cxx
+++ gui/tools/ComputeDepthTool.cxx
@@ -34,7 +34,6 @@
 #include <maptk/version.h>
 
 #include <vital/algo/image_io.h>
-#include <vital/algo/convert_image.h>
 #include <vital/algo/compute_depth.h>
 
 #include <vital/config/config_block_io.h>
@@ -50,11 +49,10 @@
 #include <QtCore/QDir>
 
 #include <algorithm>
+#include <sstream>
 
 using kwiver::vital::algo::image_io;
 using kwiver::vital::algo::image_io_sptr;
-using kwiver::vital::algo::convert_image;
-using kwiver::vital::algo::convert_image_sptr;
 using kwiver::vital::algo::compute_depth;
 using kwiver::vital::algo::compute_depth_sptr;
 using kwiver::vital::algo::video_input;
@@ -63,7 +61,6 @@ using kwiver::vital::algo::video_input_sptr;
 namespace
 {
 static char const* const BLOCK_VR = "video_reader";
-static char const* const BLOCK_CI = "image_converter";
 static char const* const BLOCK_CD = "compute_depth";
 
 //-----------------------------------------------------------------------------
@@ -90,7 +87,6 @@ class ComputeDepthToolPrivate
 {
 public:
   video_input_sptr video_reader;
-  convert_image_sptr image_converter;
   compute_depth_sptr depth_algo;
 };
 
@@ -139,19 +135,25 @@ bool ComputeDepthTool::execute(QWidget* window)
     return false;
   }
 
-  if (!image_io::check_nested_algo_configuration(BLOCK_VR, config) ||
-      !convert_image::check_nested_algo_configuration(BLOCK_CI, config) ||
-      !compute_depth::check_nested_algo_configuration(BLOCK_CD, config))
+  config->merge_config(this->data()->config);
+  if (!video_input::check_nested_algo_configuration(BLOCK_VR, config))
   {
     QMessageBox::critical(
       window, "Configuration error",
-      "An error was found in the algorithm configuration.");
+      "An error was found in the video_input configuration.");
+    return false;
+  }
+
+  if(!compute_depth::check_nested_algo_configuration(BLOCK_CD, config))
+  {
+    QMessageBox::critical(
+      window, "Configuration error",
+      "An error was found in the compute_depth configuration.");
     return false;
   }
 
   // Create algorithm from configuration
   video_input::set_nested_algo_configuration(BLOCK_VR, this->data()->config, d->video_reader);
-  convert_image::set_nested_algo_configuration(BLOCK_CI, config, d->image_converter);
   compute_depth::set_nested_algo_configuration(BLOCK_CD, config, d->depth_algo);
 
   return AbstractTool::execute(window);
@@ -163,14 +165,14 @@ void ComputeDepthTool::run()
   QTE_D();
 
   int frame = this->activeFrame();
-  landmark_map_sptr landmarks_map = this->landmarks();
-  camera_map_sptr cameras_map = this->cameras();
+  auto const& lm = this->landmarks()->landmarks();
+  auto const& cm = this->cameras()->cameras();
 
   d->video_reader->open(this->data()->videoPath);
   
-  std::vector<kwiver::vital::image_container_sptr> frames;
-  std::vector<kwiver::vital::camera_sptr> cameras;
-  std::vector<kwiver::vital::landmark_sptr> landmarks;
+  std::vector<kwiver::vital::image_container_sptr> frames_out;
+  std::vector<kwiver::vital::camera_sptr> cameras_out;
+  std::vector<kwiver::vital::landmark_sptr> landmarks_out;
   const unsigned int numsupport = 10;
   int halfsupport = numsupport / 2;
 
@@ -190,29 +192,34 @@ void ComputeDepthTool::run()
     start -= end - d->video_reader->num_frames() + 1;
     end = d->video_reader->num_frames() - 1;
   }
-  
+
   kwiver::vital::timestamp currentTimestamp;
-  d->video_reader->seek_frame(currentTimestamp, start - 1);
+  d->video_reader->seek_frame(currentTimestamp, start);
 
   //collect images and cameras
   for (int i = start; i <= end; ++i)
   {
     auto const image = d->video_reader->frame_image();
-    auto const converted_image = d->image_converter->convert(image);
-
-    frames.push_back(converted_image);
-    cameras.push_back(cameras_map->cameras()[i]);
+    frames_out.push_back(image);
+    cameras_out.push_back(cm.find(i)->second);
     d->video_reader->next_frame(currentTimestamp);
   }
-
+  
   //convert landmarks to vector
-  for (kwiver::vital::landmark_map::map_landmark_t::const_iterator itr = landmarks_map->landmarks().begin(); itr != landmarks_map->landmarks().end(); itr++)
+  foreach(auto const& l, lm)
   {
-    landmarks.push_back(itr->second);
+    QMessageBox Msgbox;
+    std::ostringstream stream;
+    stream << this->landmarks()->size() << " " << this->landmarks()->landmarks().size() << " " << l.first;
+    Msgbox.setText(QString(stream.str().c_str()));
+    Msgbox.exec();
+    landmarks_out.push_back(l.second);
   }
+  return;
+
 
   //compute depth
-  kwiver::vital::image_container_sptr depth = d->depth_algo->compute(frames, cameras, landmarks, frame);
+  kwiver::vital::image_container_sptr depth = d->depth_algo->compute(frames_out, cameras_out, landmarks_out, frame);
 
   // make a copy of the tool data
   auto data = std::make_shared<ToolData>();
