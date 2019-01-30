80659c89853b5bb9d84e3ced481c6e26bc68240e
Matt Leotta <matt.leotta@kitware.com>
2019-01-30 10:42:54 -0500

    Update CMake in Travis CI

--- .travis/install-deps.sh
+++ .travis/install-deps.sh
@@ -14,8 +14,8 @@ mkdir -p $HOME/install
 # check if directory is cached
 if [ ! -f "$INSTALL_DIR/bin/cmake" ]; then
   cd /tmp
-  wget --no-check-certificate https://cmake.org/files/v3.4/cmake-3.4.0-Linux-x86_64.sh
-  bash cmake-3.4.0-Linux-x86_64.sh --skip-license --prefix="$INSTALL_DIR/"
+  wget --no-check-certificate https://cmake.org/files/v3.9/cmake-3.9.6-Linux-x86_64.sh
+  bash cmake-3.9.6-Linux-x86_64.sh --skip-license --prefix="$INSTALL_DIR/"
 else
   echo 'Using cached CMake directory.';
 fi
