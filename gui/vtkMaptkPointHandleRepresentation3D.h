14a9ae4d340f5fb950eb7035f7a2026b2f94432d
Sankhesh Jhaveri <sankhesh.jhaveri@kitware.com>
2019-02-14 13:43:20 -0500

    Update copyright notice on custom vtk class

--- gui/vtkMaptkPointHandleRepresentation3D.h
+++ gui/vtkMaptkPointHandleRepresentation3D.h
@@ -1,5 +1,5 @@
 /*ckwg +29
- * Copyright 2018 by Kitware, Inc.
+ * Copyright 2018-2019 by Kitware, Inc.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -61,4 +61,4 @@ private:
   void operator=(const vtkMaptkPointHandleRepresentation3D) = delete;
 };
 
-#endif // vtkMaptkPointHandleRepresentation3D_h
+#endif
