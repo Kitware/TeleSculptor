46dba9463faa6784e513c445033412fb2d340e89
Matt Leotta <matt.leotta@kitware.com>
2019-03-11 22:01:13 -0400

    update copyright year

--- gui/vtkMaptkCameraRepresentation.cxx
+++ gui/vtkMaptkCameraRepresentation.cxx
@@ -1,5 +1,5 @@
 /*ckwg +29
- * Copyright 2016-2017 by Kitware, Inc.
+ * Copyright 2016-2019 by Kitware, Inc.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
