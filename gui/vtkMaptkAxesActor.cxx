264196f66846650e10ddf2ab69922e9ca5e02556
Sankhesh Jhaveri <sankhesh.jhaveri@kitware.com>
2019-10-22 09:33:38 -0400

    Add a custom axes representation for constrained motion of handles

---
+++ gui/vtkMaptkAxesActor.cxx
@@ -0,0 +1,257 @@
+/*ckwg +29
+ * Copyright 2018-2019 by Kitware, Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *  * Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ *  * Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ *  * Neither the name Kitware, Inc. nor the names of any contributors may be
+ *    used to endorse or promote products derived from this software without
+ *    specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ``AS IS''
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHORS OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+// TeleSculptor includes
+#include "vtkMaptkAxesActor.h"
+
+// VTK includes
+#include <vtkActor.h>
+#include <vtkCylinderSource.h>
+#include <vtkDiskSource.h>
+#include <vtkObjectFactory.h>
+#include <vtkPolyDataMapper.h>
+#include <vtkProperty.h>
+#include <vtkSphereSource.h>
+#include <vtkTransform.h>
+
+vtkStandardNewMacro(vtkMaptkAxesActor);
+
+//-----------------------------------------------------------------------------
+vtkMaptkAxesActor::vtkMaptkAxesActor()
+{
+  this->CylinderSource = vtkCylinderSource::New();
+  this->CylinderSource->SetRadius(0.05);
+  this->CylinderSource->SetHeight(1.5);
+  this->CylinderSource->SetResolution(16);
+
+  vtkPolyDataMapper* shaftMapper = vtkPolyDataMapper::New();
+  shaftMapper->SetInputConnection(this->CylinderSource->GetOutputPort());
+  this->ZAxisShaft = vtkActor::New();
+  this->ZAxisShaft->SetMapper(shaftMapper);
+  this->ZAxisShaft->GetProperty()->SetColor(0, 0, 1);
+  shaftMapper->Delete();
+
+  this->SphereSource = vtkSphereSource::New();
+  this->SphereSource->SetThetaResolution(16);
+  this->SphereSource->SetPhiResolution(16);
+  this->SphereSource->SetRadius(0.5);
+
+  vtkPolyDataMapper* tipMapper = vtkPolyDataMapper::New();
+  tipMapper->SetInputConnection(this->SphereSource->GetOutputPort());
+  this->ZAxisTip = vtkActor::New();
+  this->ZAxisTip->SetMapper(tipMapper);
+  this->ZAxisTip->GetProperty()->SetColor(0, 0, 1);
+  tipMapper->Delete();
+}
+
+//-----------------------------------------------------------------------------
+vtkMaptkAxesActor::~vtkMaptkAxesActor()
+{
+  this->CylinderSource->Delete();
+  this->SphereSource->Delete();
+  this->ZAxisShaft->Delete();
+  this->ZAxisTip->Delete();
+}
+
+//----------------------------------------------------------------------------
+void vtkMaptkAxesActor::PrintSelf(ostream& os, vtkIndent indent)
+{
+  os << indent
+     << "ZAxisVisibility = " << (this->ZAxisVisibility ? "True" : "False")
+     << endl;
+  os << indent << "AxesLength = " << this->AxesLength << endl;
+  this->Superclass::PrintSelf(os, indent);
+}
+
+//----------------------------------------------------------------------------
+void vtkMaptkAxesActor::GetBounds(double bounds[6])
+{
+  const double* bds = this->GetBounds();
+  bounds[0] = bds[0];
+  bounds[1] = bds[1];
+  bounds[2] = bds[2];
+  bounds[3] = bds[3];
+  bounds[4] = bds[4];
+  bounds[5] = bds[5];
+}
+
+//----------------------------------------------------------------------------
+// Get the bounds for this Actor as (Xmin,Xmax,Ymin,Ymax,Zmin,Zmax).
+double* vtkMaptkAxesActor::GetBounds()
+{
+  double bounds[6];
+  int i;
+
+  this->ZAxisShaft->GetBounds(bounds);
+  for (i = 0; i < 3; ++i)
+  {
+    this->Bounds[2 * i + 1] = (bounds[2 * i + 1] > this->Bounds[2 * i + 1])
+      ? (bounds[2 * i + 1])
+      : (this->Bounds[2 * i + 1]);
+  }
+
+  this->ZAxisTip->GetBounds(bounds);
+  for (i = 0; i < 3; ++i)
+  {
+    this->Bounds[2 * i + 1] = (bounds[2 * i + 1] > this->Bounds[2 * i + 1])
+      ? (bounds[2 * i + 1])
+      : (this->Bounds[2 * i + 1]);
+  }
+
+  // We want this actor to rotate / re-center about the origin, so give it
+  // the bounds it would have if the axes were symmetric.
+  for (i = 0; i < 3; ++i)
+  {
+    this->Bounds[2 * i] = -this->Bounds[2 * i + 1];
+  }
+
+  return this->Bounds;
+}
+
+//----------------------------------------------------------------------------
+void vtkMaptkAxesActor::UpdateProps()
+{
+  if (this->GetUserTransform())
+  {
+    this->ZAxisShaft->SetUserTransform(nullptr);
+    this->ZAxisTip->SetUserTransform(nullptr);
+  }
+
+  this->ZAxisShaft->SetVisibility(this->GetZAxisVisibility());
+  this->ZAxisTip->SetVisibility(this->GetZAxisVisibility());
+
+  if (this->GetZAxisVisibility())
+  {
+    // 70% shaft
+    double shaftLength = 0.7 * this->AxesLength;
+
+    double pos[3];
+    this->GetPosition(pos);
+
+    this->ZAxisShaft->SetScale(shaftLength, shaftLength, shaftLength);
+    this->ZAxisShaft->SetPosition(pos[0], pos[1], pos[2] + shaftLength / 2);
+    this->ZAxisShaft->SetOrientation(90, 0, 0);
+
+    // 30% tip
+    double tipLength = 0.3 * this->AxesLength;
+
+    this->ZAxisTip->SetScale(tipLength, tipLength, tipLength);
+    this->ZAxisTip->SetPosition(pos[0], pos[1], pos[2] + this->AxesLength);
+    this->ZAxisTip->SetOrientation(90, 0, 0);
+  }
+}
+
+//----------------------------------------------------------------------------
+int vtkMaptkAxesActor::RenderOpaqueGeometry(vtkViewport* vp)
+{
+  int renderedSomething = 0;
+
+  this->UpdateProps();
+
+  if (this->ZAxisVisibility)
+  {
+    renderedSomething += this->ZAxisShaft->RenderOpaqueGeometry(vp);
+    renderedSomething += this->ZAxisTip->RenderOpaqueGeometry(vp);
+  }
+
+  renderedSomething = (renderedSomething > 0) ? (1) : (0);
+  return renderedSomething;
+}
+
+//----------------------------------------------------------------------------
+int vtkMaptkAxesActor::RenderTranslucentPolygonalGeometry(vtkViewport* vp)
+{
+  int renderedSomething = 0;
+
+  this->UpdateProps();
+
+  if (this->ZAxisVisibility)
+  {
+    renderedSomething +=
+      this->ZAxisShaft->RenderTranslucentPolygonalGeometry(vp);
+    renderedSomething += this->ZAxisTip->RenderTranslucentPolygonalGeometry(vp);
+  }
+
+  renderedSomething = (renderedSomething > 0) ? (1) : (0);
+  return renderedSomething;
+}
+//----------------------------------------------------------------------------
+vtkTypeBool vtkMaptkAxesActor::HasTranslucentPolygonalGeometry()
+{
+  vtkTypeBool result = 0;
+
+  this->UpdateProps();
+
+  if (this->ZAxisVisibility)
+  {
+    result |= this->ZAxisShaft->HasTranslucentPolygonalGeometry();
+    result |= this->ZAxisTip->HasTranslucentPolygonalGeometry();
+  }
+
+  return result;
+}
+
+//----------------------------------------------------------------------------
+int vtkMaptkAxesActor::RenderOverlay(vtkViewport* vp)
+{
+  int renderedSomething = 0;
+
+  this->UpdateProps();
+
+  if (this->ZAxisVisibility)
+  {
+    renderedSomething += this->ZAxisShaft->RenderOverlay(vp);
+    renderedSomething += this->ZAxisTip->RenderOverlay(vp);
+  }
+
+  renderedSomething = (renderedSomething > 0) ? (1) : (0);
+  return renderedSomething;
+}
+
+//----------------------------------------------------------------------------
+void vtkMaptkAxesActor::ReleaseGraphicsResources(vtkWindow* win)
+{
+  this->ZAxisShaft->ReleaseGraphicsResources(win);
+  this->ZAxisTip->ReleaseGraphicsResources(win);
+}
+
+//----------------------------------------------------------------------------
+void vtkMaptkAxesActor::ShallowCopy(vtkProp* prop)
+{
+  vtkMaptkAxesActor* m = vtkMaptkAxesActor::SafeDownCast(prop);
+  if (m)
+  {
+    this->SetZAxisVisibility(m->GetZAxisVisibility());
+    this->SetAxesLength(m->GetAxesLength());
+  }
+
+  // Forward to superclass
+  Superclass::ShallowCopy(prop);
+}
