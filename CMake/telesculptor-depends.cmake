f92b05e1d6920aa074678a1d5f8eb6118a9868c9
Matt Leotta <matt.leotta@kitware.com>
2021-01-20 17:10:29 -0500

    No longer need to explicitly include/link KWIVER dirs

--- CMake/telesculptor-depends.cmake
+++ CMake/telesculptor-depends.cmake
@@ -5,8 +5,6 @@
 message(STATUS "Looking for kwiver in : ${kwiver_DIR}")
 
 find_package(kwiver REQUIRED)
-include_directories(SYSTEM "${KWIVER_INCLUDE_DIRS}")
-link_directories("${KWIVER_LIBRARY_DIRS}")
 if ( IS_DIRECTORY ${fletch_DIR} )
   find_package(fletch REQUIRED)
   message(STATUS "Using fletch from : ${fletch_DIR}")
