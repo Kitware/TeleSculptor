4b19376dc80306a8cb19ac085799b3d132409e03
Matt Leotta <matt.leotta@kitware.com>
2021-03-03 14:40:11 -0500

    removed code that has relocated to KWIVER

--- gui/tools/FuseDepthTool.cxx
+++ gui/tools/FuseDepthTool.cxx
@@ -38,6 +38,9 @@
 #include <vital/types/metadata.h>
 #include <vital/types/vector.h>
 
+#include <arrows/mvg/sfm_utils.h>
+#include <arrows/vtk/depth_utils.h>
+
 #include <qtStlUtil.h>
 #include <QMessageBox>
 
@@ -147,118 +150,6 @@ bool FuseDepthTool::execute(QWidget* window)
 }
 
 //-----------------------------------------------------------------------------
-void load_depth_map(const std::string &filename,
-                    int &i0, int &ni, int &j0, int &nj,
-                    kwiver::vital::image_container_sptr &depth_out,
-                    kwiver::vital::image_container_sptr &weight_out)
-{
-  vtkNew<vtkXMLImageDataReader> depthReader;
-  depthReader->SetFileName(filename.c_str());
-  depthReader->Update();
-  vtkImageData *img = depthReader->GetOutput();
-
-  vtkIntArray* crop = static_cast<vtkIntArray*>(
-    img->GetFieldData()->GetArray("Crop"));
-  if (crop)
-  {
-    i0 = crop->GetValue(0);
-    ni = crop->GetValue(1);
-    j0 = crop->GetValue(2);
-    nj = crop->GetValue(3);
-  }
-  else
-  {
-    i0 = j0 = 0;
-    ni = img->GetDimensions()[0];
-    nj = img->GetDimensions()[1];
-  }
-
-  vtkDoubleArray *depths = dynamic_cast<vtkDoubleArray *>(
-    img->GetPointData()->GetArray("Depths"));
-  vtkDoubleArray *weights = dynamic_cast<vtkDoubleArray*>(
-    img->GetPointData()->GetArray("Weight"));
-
-  int dims[3];
-  img->GetDimensions(dims);
-
-  kwiver::vital::image_of<double> depth(dims[0], dims[1], dims[2]);
-  kwiver::vital::image_of<double> weight(dims[0], dims[1], dims[2]);
-
-  vtkIdType pt_id = 0;
-  for (int x = 0; x < dims[0]; x++)
-  {
-    for (int y = 0; y < dims[1]; y++)
-    {
-      depth(x, y) = depths->GetValue(pt_id);
-      weight(x, y) = weights ? weights->GetValue(pt_id) : 1.0;
-      pt_id++;
-    }
-  }
-
-  depth_out = std::make_shared<kwiver::vital::simple_image_container>(depth);
-  weight_out = std::make_shared<kwiver::vital::simple_image_container>(weight);
-}
-
-//-----------------------------------------------------------------------------
-vtkSmartPointer<vtkImageData>
-volume_to_vtk(kwiver::vital::image_container_sptr volume,
-              kwiver::vital::vector_3d const& origin,
-              kwiver::vital::vector_3d const& spacing)
-{
-  vtkSmartPointer<vtkImageData> grid = vtkSmartPointer<vtkImageData>::New();
-  grid->SetOrigin(origin[0], origin[1], origin[2]);
-  grid->SetDimensions(static_cast<int>(volume->width()),
-                      static_cast<int>(volume->height()),
-                      static_cast<int>(volume->depth()));
-  grid->SetSpacing(spacing[0], spacing[1], spacing[2]);
-
-  // initialize output
-  vtkNew<vtkDoubleArray> vals;
-  vals->SetName("reconstruction_scalar");
-  vals->SetNumberOfComponents(1);
-  vals->SetNumberOfTuples(volume->width() * volume->height() * volume->depth());
-
-  vtkIdType pt_id = 0;
-  const kwiver::vital::image &vol = volume->get_image();
-
-  for (unsigned int k = 0; k < volume->depth(); k++)
-  {
-    for (unsigned int j = 0; j < volume->height(); j++)
-    {
-      for (unsigned int i = 0; i < volume->width(); i++)
-      {
-        vals->SetTuple1(pt_id++, vol.at<double>(i, j, k));
-      }
-    }
-  }
-
-  grid->GetPointData()->SetScalars(vals);
-  grid->GetPointData()->GetAbstractArray(0)->SetName("reconstruction_scalar");
-  return grid;
-}
-
-//-----------------------------------------------------------------------------
-kwiver::vital::camera_perspective_sptr
-crop_camera(const kwiver::vital::camera_perspective_sptr& cam,
-            int i0, int ni, int j0, int nj)
-{
-  kwiver::vital::simple_camera_intrinsics newIntrinsics(*cam->intrinsics());
-  kwiver::vital::vector_2d pp = newIntrinsics.principal_point();
-
-  pp[0] -= i0;
-  pp[1] -= j0;
-
-  newIntrinsics.set_principal_point(pp);
-
-  kwiver::vital::simple_camera_perspective cropCam(cam->center(),
-                                                   cam->rotation(),
-                                                   newIntrinsics);
-
-  return std::dynamic_pointer_cast<kwiver::vital::camera_perspective>(
-    cropCam.clone());
-}
-
-//-----------------------------------------------------------------------------
 void FuseDepthTool::run()
 {
   using namespace kwiver::vital;
@@ -279,12 +170,13 @@ void FuseDepthTool::run()
       continue;
     camera_perspective_sptr cam =
       std::dynamic_pointer_cast<camera_perspective>(camitr->second);
-    int i0, ni, j0, nj;
-    image_container_sptr depth, weight;
-    load_depth_map(item.second, i0, ni, j0, nj, depth, weight);
+    bounding_box<int> crop;
+    image_container_sptr depth, weight, uncertainty, color;
+    kwiver::arrows::vtk::load_depth_map(
+      item.second, crop, depth, weight, uncertainty, color);
     depths_out.push_back(depth);
     weights_out.push_back(weight);
-    cameras_out.push_back(crop_camera(cam, i0, ni, j0, nj));
+    cameras_out.push_back(kwiver::arrows::mvg::crop_camera(cam, crop));
   }
 
   double minptd[3];
@@ -302,7 +194,7 @@ void FuseDepthTool::run()
                           volume, spacing);
 
   vtkSmartPointer<vtkImageData> vtk_volume =
-    volume_to_vtk(volume, minpt, spacing);
+    kwiver::arrows::vtk::volume_to_vtk(volume, minpt, spacing);
 
   this->updateFusion(vtk_volume);
 }
